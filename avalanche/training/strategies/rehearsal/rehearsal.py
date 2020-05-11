5340dc05e9c3a0890a877f6c5d0d507e40747601
Vincenzo Lomonaco <vincenzo.lomonaco@unibo.it>
2020-05-11 15:26:03 +0200

    skeleton directory removed

--- avalanche/training/strategies/rehearsal/rehearsal.py
+++ avalanche/training/strategies/rehearsal/rehearsal.py
@@ -19,7 +19,7 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
-from avalanche.training.strategies.skeletons.strategy \
+from training.strategies.strategy \
     import Strategy
 from avalanche.evaluation.eval_protocol import EvalProtocol
 from avalanche.evaluation.metrics import ACC
