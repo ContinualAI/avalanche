03e8254eacd154aec4d35b1215eeebc3683413a7
AndreaCossu <andrea.cossu@sns.it>
2020-05-20 17:28:09 +0200

    Added TinyImageNet together with example and test

---
+++ avalanche/extras/models/mlp_tiny_imagenet.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+
+class SimpleMLP_TinyImageNet(nn.Module):
+
+    def __init__(self, num_classes=200, num_channels=3):
+        super(SimpleMLP_TinyImageNet, self).__init__()
+
+        self.features = nn.Sequential(
+            nn.Linear(num_channels*64*64, 1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+        )
+        self.classifier = nn.Linear(1024, num_classes)
+
+    def forward(self, x):
+        x = x.contiguous()
+        x = x.view(x.size(0), -1)
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
