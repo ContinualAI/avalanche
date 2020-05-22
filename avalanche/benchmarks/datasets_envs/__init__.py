9e5f719378d58c1c1bfb3ed9a23bfafc3062d747
Prashant Rai <prashant81995@gmail.com>
2020-05-23 00:53:11 +0530

    updated init

--- avalanche/benchmarks/datasets_envs/__init__.py
+++ avalanche/benchmarks/datasets_envs/__init__.py
@@ -1,7 +1,4 @@
 from .mnist import MNIST
 from .core50 import CORE50_DATA
-<<<<<<< Updated upstream
 from .imagenet import ImageNet
 from .tiny_imagenet import TinyImageNet_data
-=======
->>>>>>> Stashed changes
