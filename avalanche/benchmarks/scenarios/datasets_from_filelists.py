e31ab896db14da2b011f9820198f667b0f51b366
Lorenzo Pellegrini <lrzpellegrini@gmail.com>
2020-06-22 18:25:02 +0200

    Implemented helper function to create CL scenarios from filelists (Caffe-style)

--- avalanche/benchmarks/scenarios/datasets_from_filelists.py
+++ avalanche/benchmarks/scenarios/datasets_from_filelists.py
@@ -17,6 +17,8 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
+from pathlib import Path
+
 import torch.utils.data as data
 
 from PIL import Image
@@ -25,116 +27,131 @@ import os.path
 
 
 def default_loader(path):
-	"""
-	Sets the default image loader for the Pytorch Dataset.
+    """
+    Sets the default image loader for the Pytorch Dataset.
 
-	:param path: relative or absolute path of the file to load.
+    :param path: relative or absolute path of the file to load.
 
-	:returns: Returns the image as a RGB PIL image.
-	"""
-	return Image.open(path).convert('RGB')
+    :returns: Returns the image as a RGB PIL image.
+    """
+    return Image.open(path).convert('RGB')
 
 
 def default_flist_reader(flist, root):
-	"""
-	This reader reads a filelist and return a list of paths.
+    """
+    This reader reads a filelist and return a list of paths.
 
-	:param flist: path of the flislist to read. The flist format should be:
-		impath label\nimpath label\n ...(same to caffe's filelist)
+    :param flist: path of the flislist to read. The flist format should be:
+        impath label\nimpath label\n ...(same to caffe's filelist)
+    :param root: path to the dataset root. Each file defined in the file list
+        will be searched in <root>/<impath>.
 
-	:returns: Returns a list of paths (the examples to be loaded).
-	"""
+    :returns: Returns a list of paths (the examples to be loaded).
+    """
 
-	imlist = []
-	with open(flist, 'r') as rf:
-		for line in rf.readlines():
-			impath, imlabel = line.strip().split()
-			imlist.append( (os.path.join(root, impath), int(imlabel)) )
-					
-	return imlist
+    imlist = []
+    with open(flist, 'r') as rf:
+        for line in rf.readlines():
+            impath, imlabel = line.strip().split()
+            imlist.append((os.path.join(root, impath), int(imlabel)))
+
+    return imlist
 
 
 class FilelistDataset(data.Dataset):
-	"""
-	This class extends the basic Pytorch Dataset class to handle filelists as
-	main data source.
-	"""
+    """
+    This class extends the basic Pytorch Dataset class to handle filelists as
+    main data source.
+    """
 
-	def __init__(self, root, flist, transform=None, target_transform=None,
-			flist_reader=default_flist_reader, loader=default_loader):
-		"""
-		This reader reads a filelist and return a list of paths.
+    def __init__(
+            self, root, flist, transform=None, target_transform=None,
+            flist_reader=default_flist_reader, loader=default_loader):
+        """
+        This reader reads a filelist and return a list of paths.
 
-		:param root: root path where the data to load are stored.
-		:param flist: path of the flislist to read. The flist format should be:
-			impath label\nimpath label\n ...(same to caffe's filelist)
-		:param transform: eventual transformation to add to the input data (x)
-		:param transform: eventual transformation to add to the targets (y)
-		:param root: root path where the data to load are stored.
-		:param flist_reader: loader function to use (for the filelists) given
-			path.
-		:param loader: loader function to use (for the real data) given path.
-		"""
+        :param root: root path where the data to load are stored.
+        :param flist: path of the flislist to read. The flist format should be:
+            impath label\nimpath label\n ...(same to caffe's filelist)
+        :param transform: eventual transformation to add to the input data (x)
+        :param transform: eventual transformation to add to the targets (y)
+        :param root: root path where the data to load are stored.
+        :param flist_reader: loader function to use (for the filelists) given
+            path.
+        :param loader: loader function to use (for the real data) given path.
+        """
+        root = str(root)  # Manages Path objects
+        flist = str(flist)  # Manages Path objects
+        self.root = root
+        self.imgs = flist_reader(flist, root)
+        self.targets = [img_data[1] for img_data in self.imgs]
+        self.transform = transform
+        self.target_transform = target_transform
+        self.loader = loader
 
-		self.root = root
-		self.imgs = flist_reader(flist, root)
-		self.targets = [img_data[1] for img_data in self.imgs]
-		self.transform = transform
-		self.target_transform = target_transform
-		self.loader = loader
+    def __getitem__(self, index):
+        """
+        Returns next element in the dataset given the current index.
 
-	def __getitem__(self, index):
-		"""
-		Returns next element in the dataset given the current index.
+        :param index: index of the data to get.
+        :return: loaded item.
+        """
 
-		:param index: index of the data to get.
-		:return: loaded item.
-		"""
+        impath, target = self.imgs[index]
+        img = self.loader(os.path.join(self.root, impath))
+        if self.transform is not None:
+            img = self.transform(img)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
 
-		impath, target = self.imgs[index]
-		img = self.loader(os.path.join(self.root,impath))
-		if self.transform is not None:
-			img = self.transform(img)
-		if self.target_transform is not None:
-			target = self.target_transform(target)
-		
-		return img, target
+        return img, target
 
-	def __len__(self):
-		"""
-		Returns the total number of elements in the dataset.
+    def __len__(self):
+        """
+        Returns the total number of elements in the dataset.
 
-		:return: Total number of dataset items.
-		"""
+        :return: Total number of dataset items.
+        """
 
-		return len(self.imgs)
+        return len(self.imgs)
 
-def datasets_from_filelists(root, train_filelists, test_fielists):
-	"""
-	This reader reads a filelist and return a list of paths.
 
-	:param root: root path where the data to load are stored.
-	:param train_filelists: list of paths to train filelists. The flist format
-		should be: impath label\nimpath label\n ...(same to caffe's filelist)
-	:param test_filelists: list of paths to test filelists. It can be also a
-		single path when the datasets is the same for each batch.
-	:return: list of tuples (train dataset, test dataset) for each train
-		filelist in the list.
-	"""
+def datasets_from_filelists(root, train_filelists, test_filelists,
+                            complete_test_set_only=False):
+    """
+    This reader reads a filelist and return a list of paths.
 
-	inc_datasets = []
+    :param root: root path where the data to load are stored.
+    :param train_filelists: list of paths to train filelists. The flist format
+        should be: impath label\nimpath label\n ...(same to caffe's filelist)
+    :param test_filelists: list of paths to test filelists. It can be also a
+        single path when the datasets is the same for each batch.
+    :param complete_test_set_only: if True, test_filelists must contain
+        the path to a single filelist that will serve as the complete test set.
+        Alternatively, test_filelists can be the path (str) to the complete test
+        set filelist. If False, train_filelists and test_filelists must contain
+        the same amount of filelists paths. Defaults to False.
+    :return: list of tuples (train dataset, test dataset) for each train
+        filelist in the list.
+    """
 
-	if not isinstance(test_fielists, list):
-		list_test_filelist = []
-		for i in range(len(train_filelists)):
-			list_test_filelist.append(test_fielists)
-		test_fielists = list_test_filelist
+    if complete_test_set_only and not (isinstance(test_filelists, str) or
+                                       isinstance(test_filelists, Path)):
+        if len(test_filelists) > 1:
+            raise ValueError(
+                'When complete_test_set_only is True, test_filelists must be a '
+                'str, Path or a list with a single element describing the path '
+                'to the complete test set.')
+        test_filelists = test_filelists[0]
+    if not complete_test_set_only:
+        if len(test_filelists) != len(train_filelists):
+            raise ValueError(
+                'When complete_test_set_only is False, test_filelists and '
+                'train_filelists must contain the same number of elements.')
 
-	for tr_flist, te_flist in zip(train_filelists, test_fielists):
-		tr_dataset = FilelistDataset(root, tr_flist)
-		te_dataset = FilelistDataset(root, te_flist)
+    train_inc_datasets = \
+        [FilelistDataset(root, tr_flist) for tr_flist in train_filelists]
+    test_inc_datasets = \
+        [FilelistDataset(root, te_flist) for te_flist in test_filelists]
 
-		inc_datasets.append((tr_dataset, te_dataset))
-
-	return inc_datasets
-
+    return train_inc_datasets, test_inc_datasets
--- avalanche/benchmarks/scenarios/generic_scenario_creation.py
+++ avalanche/benchmarks/scenarios/generic_scenario_creation.py
@@ -1,7 +1,9 @@
-from typing import Sequence
+from pathlib import Path
+from typing import Sequence, Union
 
 from avalanche.training.utils import IDatasetWithTargets, \
     ConcatDatasetWithTargets
+from .datasets_from_filelists import datasets_from_filelists
 from .generic_cl_scenario import GenericCLScenario
 
 
@@ -35,10 +37,9 @@ def create_multi_dataset_generic_scenario(
         be returned by the scenario. This means that the ``test_dataset_list``
         parameter must be list with a single element (the complete test set).
         Defaults to False, which means that ``train_dataset_list`` and
-       ` `test_dataset_list`` must contain the same amount of datasets.
+        ``test_dataset_list`` must contain the same amount of datasets.
 
-    :returns: A :class:`NCMultiTaskScenario` instance initialized for the
-        the SIT scenario.
+    :returns: A :class:`GenericCLScenario` instance.
     """
 
     # GenericCLScenario accepts a single training+test sets couple along with
@@ -83,4 +84,57 @@ def create_multi_dataset_generic_scenario(
         task_labels, return_complete_test_set_only=complete_test_set_only)
 
 
-__all__ = ['create_multi_dataset_generic_scenario']
+def create_generic_scenario_from_filelists(
+        root: Union[str, Path],
+        train_file_lists: Sequence[Union[str, Path]],
+        test_file_lists: Union[Union[str, Path], Sequence[Union[str, Path]]],
+        task_labels: Sequence[int],
+        complete_test_set_only: bool = False) -> GenericCLScenario:
+    """
+    Creates a generic scenario given a list of filelists and the respective task
+    labels. A separate dataset will be created for each filelist and each of
+    those training datasets will be considered a separate training step.
+    Contents of the datasets will not be changed, including the targets.
+
+    In its base form, this function accepts a list of filelists for the test
+    datsets that must contain the same amount of elements of the training list.
+    Those pairs of datasets are then used to create the "past", "cumulative"
+    (a.k.a. growing) and "future" test sets. However, in certain Continual
+    Learning scenarios only the concept of "complete" test set makes sense. In
+    that case, the ``complete_test_set_only`` should be set to True.
+    See the parameter description for more info.
+
+    This helper functions is the best shot when loading Caffe-style dataset
+    based on filelists.
+
+    :param root: The root path of the dataset.
+    :param train_file_lists: A list of filelists describing the
+        paths of the training patterns for each step.
+    :param test_file_lists: A list of filelists describing the
+        paths of the test patterns for each step.
+    :param task_labels: A list of task labels. Must contain the same amount of
+        elements of the ``train_file_lists`` parameter. For
+        Single-Incremental-Task (a.k.a. Task-Free) scenarios, this is usually
+        a list of zeros. For Multi Task scenario, this is usually a list of
+        ascending task labels (starting from 0).
+    :param complete_test_set_only: If True, only the complete test set will
+        be returned by the scenario. This means that the ``test_file_lists``
+        parameter must be list with a single element (the complete test set).
+        Alternatively, can be a plain string or :class:`Path` object.
+        Defaults to False, which means that ``train_file_lists`` and
+        ``test_file_lists`` must contain the same amount of filelists paths.
+
+    :returns: A :class:`GenericCLScenario` instance.
+    """
+
+    train_datasets, test_dataset = \
+        datasets_from_filelists(root, train_file_lists, test_file_lists,
+                                complete_test_set_only=complete_test_set_only)
+
+    return create_multi_dataset_generic_scenario(
+        train_datasets, test_dataset, task_labels,
+        complete_test_set_only=complete_test_set_only)
+
+
+__all__ = ['create_multi_dataset_generic_scenario',
+           'create_generic_scenario_from_filelists']
