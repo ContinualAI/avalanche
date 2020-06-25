d9f78f8a600842a355cb6033a89a272fa3f01c65
Gabriele Graffieti <gabriele.graffieti@unibo.it>
2020-06-25 23:17:35 +0200

    add cifar100 + pretrain with cifar10 in batch zero scenario creator helper

--- avalanche/benchmarks/new_cdata_loaders/icifar100.py
+++ avalanche/benchmarks/new_cdata_loaders/icifar100.py
@@ -14,13 +14,22 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
+import random
 from typing import Sequence, Optional
 from torchvision.datasets import CIFAR100
 from torchvision import transforms
+from avalanche.benchmarks.scenarios.new_classes.nc_generic_scenario \
+    import NCGenericScenario
+from avalanche.benchmarks.scenarios.new_classes.nc_scenario \
+    import NCMultiTaskScenario, NCSingleTaskScenario
+from avalanche.training.utils.transform_dataset import \
+    concat_datasets_sequentially
 from avalanche.benchmarks.scenarios.new_classes.scenario_creation import \
     create_nc_single_dataset_sit_scenario, \
     create_nc_single_dataset_multi_task_scenario
-from avalanche.benchmarks.scenarios.generic_cl_scenario import GenericCLScenario
+from avalanche.benchmarks.scenarios.new_classes.nc_scenario import \
+    NCSingleTaskScenario
+from avalanche.benchmarks.new_cdata_loaders.icifar10 import __download_cifar10
 
 
 def create_cifar100_benchmark(incremental_steps: int,
@@ -85,6 +94,65 @@ def create_cifar100_benchmark(incremental_steps: int,
             fixed_class_order=fixed_class_order,
             per_batch_classes={0: 50} if first_batch_with_half_classes else None
         )
+
+
+def create_cifar100_with_cifar10_pretrain_benchmark(
+        incremental_steps: int,
+        seed: Optional[int] = None,
+        fixed_class_order: Optional[Sequence[int]] = None) \
+        -> NCSingleTaskScenario:
+
+    """
+    Creates a Single Incremental Task (SIT) scenario using the CIFAR100 dataset,
+    with a pretrain first batch using CIFAR10.
+    If the datasets are not present in the computer the method automatically
+    download them and store the data in the data folder.
+
+    :param incremental_steps: The number of incremental steps for the
+        incremental training on cifar100. The first "pretrain" batch using
+        cifar10 is not included in this number of incremental steps.
+    :param seed: A valid int used to initialize the random number generator.
+        Can be None.
+    :param fixed_class_order: A list of class IDs used to define the class
+        order ONLY for the incremental part on cifar100. The classes must be in
+        range 0-99.
+        If None, value of ``seed`` will be used to define the class
+        order for the incremental batches on cifar100.
+        If non-None, ``seed`` parameter will be ignored.
+        Defaults to None.
+
+    :returns: A :class:`NCSingleTaskScenario` instance initialized for the the
+        SIT scenario using CIFAR10 as a pretrain batch zero and CIFAR100 for the
+        incremental training.  
+    """
+
+    cifar10_train, cifar10_test = __download_cifar10()
+    cifar100_train, cifar100_test = __download_cifar100()
+    cifar_10_100_train, cifar_10_100_test, _ = concat_datasets_sequentially(
+        [cifar10_train, cifar100_train], [cifar10_test, cifar100_test]
+    )
+    # cifar10 classes
+    class_order = [_ for _ in range(10)]
+    # if a class order is defined (for cifar100) the given class labels are
+    # appended to the class_order list, adding 10 to them (since the classes
+    # 0-9 are the classes of cifar10).
+    if fixed_class_order is not None:
+        class_order.extend([c + 10 for c in fixed_class_order])
+    else:
+        random.seed(seed)
+        # random shuffling of the cifar100 classes (labels 10-109)
+        cifar_100_class_order = random.sample(range(10, 110), 100)
+        class_order.extend(cifar_100_class_order)
+
+    base_scenario = NCGenericScenario(
+        cifar_10_100_train, cifar_10_100_test,
+        n_batches=incremental_steps + 1,
+        shuffle=False,
+        seed=None,
+        fixed_class_order=class_order,
+        per_batch_classes={0: 10})
+
+    return NCSingleTaskScenario(base_scenario)
 
 
 def __download_cifar100():
