18ce09ad249500602f89b8d0a9cd39aae3e59a67
Gabriele Graffieti <gabriele.graffieti@unibo.it>
2020-06-25 16:57:16 +0200

    edit icifar100 helper code to generate the MT and the SIT version with the same function

--- avalanche/benchmarks/new_cdata_loaders/icifar100.py
+++ avalanche/benchmarks/new_cdata_loaders/icifar100.py
@@ -20,34 +20,39 @@ from torchvision import transforms
 from avalanche.benchmarks.scenarios.new_classes.scenario_creation import \
     create_nc_single_dataset_sit_scenario, \
     create_nc_single_dataset_multi_task_scenario
-from avalanche.benchmarks.scenarios.new_classes.nc_scenario \
-    import NCMultiTaskScenario, NCSingleTaskScenario
+from avalanche.benchmarks.scenarios.generic_cl_scenario import GenericCLScenario
 
 
-def create_multi_task_cifar100(incremental_tasks: int,
-                               first_task_with_half_classes: bool = False,
-                               seed: Optional[int] = None,
-                               fixed_class_order: Optional[Sequence[int]] = None
-                               ) -> NCMultiTaskScenario:
+def create_cifar100_benchmark(incremental_steps: int,
+                              first_batch_with_half_classes: bool = False,
+                              return_task_id=False,
+                              seed: Optional[int] = None,
+                              fixed_class_order: Optional[Sequence[int]] = None
+                              ):
     """
-    Creates a "New Classes - Multi Task" scenario using the CIFAR100 dataset.
+    Creates a CL scenario using the CIFAR100 dataset.
     If the dataset is not present in the computer the method automatically
     download it and store the data in the data folder.
 
-    :param incremental_tasks: The number of incremental tasks in the current
-        scenario. If the first task is a "pretrain" task and it contains
-        half of the classes, the number of incremental tasks is the number of
+    :param incremental_steps: The number of incremental steps in the current
+        scenario. If the first step is a "pretrain" step and it contains
+        half of the classes, the number of incremental steps is the number of
         tasks performed after the pretraining task.
         The value of this parameter should be a divisor of 100 if
         first_task_with_half_classes if false, a divisor of 50 otherwise.
-    :param first_task_with_half_classes: A boolean value that indicates if a
-        first pretraining task containing half of the classes should be used.
-        If it's True, a pretrain task with half of the classes (50 for
+    :param first_batch_with_half_classes: A boolean value that indicates if a
+        first pretraining batch containing half of the classes should be used.
+        If it's True, a pretrain batch with half of the classes (50 for
         cifar100) is used, and a number of incremental tasks, given by the
         parameter incremental_task is constructed. If this paramenter is False
         no pretraining task will be used, and the dataset is simply split into
-        a the number of tasks defined by the parameter incremental_tasks.
+        a the number of steps defined by the parameter incremental_steps.
         Default to False.
+    :param return_task_id: if True, for every step the task id is returned and
+        the Scenario is Multi Task. This means that the scenario returned
+        will be of type ``NCMultiTaskScenario``. If false the task index is
+        not returned (default to 0 for every batch) and the returned scenario
+        is of type ``NCSingleTaskScenario``.
     :param seed: A valid int used to initialize the random number generator.
         Can be None.
     :param fixed_class_order: A list of class IDs used to define the class
@@ -56,68 +61,30 @@ def create_multi_task_cifar100(incremental_tasks: int,
         Defaults to None.
 
     :returns: A :class:`NCMultiTaskScenario` instance initialized for the the
-        MT scenario using CIFAR100.
+        MT scenario using CIFAR100 if the parameter ``return_task_id`` is True,
+        a :class:`NCSingleTaskScenario` initialized for the SIT scenario using
+        CIFAR100 otherwise.
     """
     cifar_train, cifar_test = __download_cifar100()
-    total_tasks = incremental_tasks + 1 if first_task_with_half_classes \
-        else incremental_tasks
-    return create_nc_single_dataset_multi_task_scenario(
-        train_dataset=cifar_train,
-        test_dataset=cifar_test,
-        n_tasks=total_tasks,
-        seed=seed,
-        fixed_class_order=fixed_class_order,
-        per_task_classes={0: 50} if first_task_with_half_classes else None
-    )
-
-
-def create_single_task_cifar100(incremental_batches: int,
-                                first_batch_with_half_classes: bool = False,
-                                seed: Optional[int] = None,
-                                fixed_class_order: Optional[Sequence[int]] =
-                                None) -> NCSingleTaskScenario:
-    """
-    Creates a "New Classes - Single Incremental Task" scenario using the
-    CIFAR100 dataset.
-    If the dataset is not present in the computer the method automatically
-    download it and store the data in the data folder.
-
-    :param incremental_batches: The number of incremental batches in the current
-        scenario. If the first batch is a "pretrain" batch and it contains
-        half of the classes, the number of incremental batches is the number of
-        batches performed after the pretraining task.
-        The value of this parameter should be a divisor of 100 if
-        first_batch_with_half_classes if false, a divisor of 50 otherwise.
-    :param first_batch_with_half_classes: A boolean value that indicates if a
-        first pretraining batch containing half of the classes should be used.
-        If it's True, a pretrain batch with half of the classes (50 for
-        cifar100) is used, and a number of incremental batches, given by the
-        parameter incremental_batches is constructed. If this paramenter is
-        False no pretraining batch will be used, and the dataset is simply
-        split into a the number of batches defined by the parameter
-        incremental_batchs.
-        Default to False.
-    :param seed: A valid int used to initialize the random number generator.
-        Can be None.
-    :param fixed_class_order: A list of class IDs used to define the class
-        order. If None, value of ``seed`` will be used to define the class
-        order. If non-None, ``seed`` parameter will be ignored.
-        Defaults to None.
-
-    :returns: A :class:`NCSingleTaskScenario` instance initialized for the the
-        SIT scenario using CIFAR100.
-    """
-    cifar_train, cifar_test = __download_cifar100()
-    total_tasks = incremental_batches + 1 if first_batch_with_half_classes \
-        else incremental_batches
-    return create_nc_single_dataset_sit_scenario(
-        train_dataset=cifar_train,
-        test_dataset=cifar_test,
-        n_batches=total_tasks,
-        seed=seed,
-        fixed_class_order=fixed_class_order,
-        per_batch_classes={0: 50} if first_batch_with_half_classes else None
-    )
+    total_steps = incremental_steps + 1 if first_batch_with_half_classes \
+        else incremental_steps
+    if return_task_id:
+        return create_nc_single_dataset_multi_task_scenario(
+            train_dataset=cifar_train,
+            test_dataset=cifar_test,
+            n_tasks=total_steps,
+            seed=seed,
+            fixed_class_order=fixed_class_order,
+            per_task_classes={0: 50} if first_batch_with_half_classes else None)
+    else:
+        return create_nc_single_dataset_sit_scenario(
+            train_dataset=cifar_train,
+            test_dataset=cifar_test,
+            n_batches=total_steps,
+            seed=seed,
+            fixed_class_order=fixed_class_order,
+            per_batch_classes={0: 50} if first_batch_with_half_classes else None
+        )
 
 
 def __download_cifar100():
