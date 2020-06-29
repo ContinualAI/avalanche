5788e99bfd3e7ec07766819f5b0fe02028b156bb
Gabriele Graffieti <gabriele.graffieti@unibo.it>
2020-06-30 01:40:30 +0200

    fix indentation bug

--- avalanche/benchmarks/new_cdata_loaders/icifar10.py
+++ avalanche/benchmarks/new_cdata_loaders/icifar10.py
@@ -27,7 +27,7 @@ _default_cifar10_train_transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465),
                              (0.2023, 0.1994, 0.2010))
-    ])
+])
 
 _default_cifar10_test_transform = transforms.Compose([
     transforms.ToTensor(),
--- avalanche/benchmarks/new_cdata_loaders/icifar100.py
+++ avalanche/benchmarks/new_cdata_loaders/icifar100.py
@@ -36,13 +36,13 @@ _default_cifar100_train_transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465),
                              (0.2023, 0.1994, 0.2010))
-    ])
+])
 
 _default_cifar100_test_transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465),
                              (0.2023, 0.1994, 0.2010))
-    ])
+])
 
 
 def create_cifar100_benchmark(incremental_steps: int,
