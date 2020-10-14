2db9da062024618a694f33ac4341e5266bebfe39
Andrea Cossu <cossu@c4130-p100.itc.unipi.it>
2020-10-14 12:38:06 +0200

    Added cumulative plugin

--- avalanche/training/plugins/__init__.py
+++ avalanche/training/plugins/__init__.py
@@ -1,2 +1,3 @@
 from .evaluation_plugin import *
 from .replay_plugin import *
+from .cumulative_plugin import *
---
+++ avalanche/training/plugins/cumulative_plugin.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+################################################################################
+# Copyright (c) 2020 ContinualAI Research                                      #
+# Copyrights licensed under the CC BY 4.0 License.                             #
+# See the accompanying LICENSE file for terms.                                 #
+#                                                                              #
+# Date: 1-06-2020                                                              #
+# Author(s): Andrea Cossu                                                #
+# E-mail: contact@continualai.org                                              #
+# Website: clair.continualai.org                                               #
+################################################################################
+
+from __future__ import absolute_import
+from __future__ import division
+# Python 2-3 compatible
+from __future__ import print_function
+
+from typing import Optional, Sequence
+
+from torch.nn import Module
+from torch.optim import Optimizer
+
+from avalanche.evaluation import EvalProtocol
+from avalanche.training.skeletons import TrainingFlow
+from avalanche.benchmarks.scenarios.generic_definitions import IStepInfo
+from avalanche.training.skeletons import StrategySkeleton
+from avalanche.training.skeletons import StrategySkeleton
+from avalanche.training.utils import ConcatDatasetWithTargets
+
+
+class CumulativePlugin(StrategySkeleton):
+    """
+    A Cumulative CL Plugin in which, at each step (or task), the model
+    is trained with all the data encountered so far. Therefore, at each step,
+    the model is trained in a MultiTask scenario.
+    The resulting strategy has a high memory and computational cost.
+    """
+
+    def __init__(self):
+        super(CumulativePlugin, self).__init__()
+
+
+    @TrainingFlow
+    def make_train_dataset(self, step_info: IStepInfo):
+        """
+        Returns the training dataset, given the step_info instance.
+        The dataset is composed by all datasets encountered so far.
+
+        This is a part of the training flow. Sets the train_dataset namespace
+        value.
+
+        :param step_info: The step info instance, as returned from the CL
+            scenario.
+        :return: The training dataset.
+        """
+
+        train_dataset = step_info.cumulative_training_sets()
+        
+        train_dataset = ConcatDatasetWithTargets(
+            [ el[0] for el in train_dataset ]
+        )
+
+        self.update_namespace(train_dataset=train_dataset)
+        self.update_namespace(step_id=step_info.current_step)
+        return train_dataset
+
+
+__all__ = ['CumulativePlugin']
