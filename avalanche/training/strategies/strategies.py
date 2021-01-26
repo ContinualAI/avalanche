0d4699c935744883fc4c44b25c052eabb952fd53
Lorenzo Pellegrini <lrzpellegrini@gmail.com>
2021-01-26 18:18:55 +0100

    Porting AR1 (work in progress).

--- avalanche/training/strategies/strategies.py
+++ avalanche/training/strategies/strategies.py
@@ -10,15 +10,17 @@
 ################################################################################
 from typing import Optional, Sequence, List, Union
 
-from torch.nn import Module
-from torch.optim import Optimizer
+from torch.nn import Module, CrossEntropyLoss
+from torch.optim import Optimizer, SGD
 
 from torch.utils.data import ConcatDataset
 
+from avalanche.models.mobilenetv1 import MobilenetV1
+from avalanche.training.deprecated.ar1.utils import replace_bn_with_brn
 from avalanche.training.strategies.base_strategy import BaseStrategy
 from avalanche.training.plugins import StrategyPlugin, \
     CWRStarPlugin, ReplayPlugin, GDumbPlugin, LwFPlugin, AGEMPlugin, \
-    GEMPlugin, EWCPlugin
+    GEMPlugin, EWCPlugin, SynapticIntelligencePlugin
 
 
 class Naive(BaseStrategy):
@@ -346,6 +348,84 @@ class EWC(BaseStrategy):
             model, optimizer, criterion,
             train_mb_size=train_mb_size, train_epochs=train_epochs,
             test_mb_size=test_mb_size, device=device, plugins=plugins)
+
+
+class AR1(BaseStrategy):
+    """
+    TODO: doc
+    The simplest (and least effective) Continual Learning strategy. Naive just
+    incrementally fine tunes a single model without employing any method
+    to contrast the catastrophic forgetting of previous knowledge.
+    This strategy does not use task identities.
+
+    Naive is easy to set up and its results are commonly used to show the worst
+    performing baseline.
+    """
+
+    def __init__(self, criterion=None, lr: float = 0.001,
+                 init_update_rate: float = 0.01, inc_update_rate=0.00005,
+                 max_r_max=1.25, max_d_max=0.5, inc_step=4.1e-05, momentum=0.9,
+                 l2=0.0005, rm_sz: int = 1500,
+                 freeze_below_layer: str = "lat_features.19.bn.beta",
+                 latent_layer_num: int = 19, ewc_lambda: float = 0,
+                 train_mb_size: int = 128,
+                 train_epochs: int = 128, test_mb_size: int = 128,
+                 device=None,
+                 plugins: Optional[Sequence[StrategyPlugin]] = None):
+        """
+        TODO: doc
+        Creates an instance of the Naive strategy.
+
+        :param model: The model.
+        :param lr:
+        :param criterion: The loss criterion to use. Defaults to None, in which
+            case the cross entropy loss is used.
+        :param train_mb_size: The train minibatch size. Defaults to 1.
+        :param train_epochs: The number of training epochs. Defaults to 1.
+        :param test_mb_size: The test minibatch size. Defaults to 1.
+        :param device: The device to use. Defaults to None (cpu).
+        :param plugins: Plugins to be added. Defaults to None.
+        """
+
+        if plugins is None:
+            plugins = []
+
+        # Model setup
+        model = MobilenetV1(pretrained=True, latent_layer_num=latent_layer_num)
+        replace_bn_with_brn(
+            model, momentum=init_update_rate, r_d_max_inc_step=inc_step,
+            max_r_max=max_r_max, max_d_max=max_d_max
+        )
+
+        model.saved_weights = {}
+        model.past_j = {i: 0 for i in range(50)}
+        model.cur_j = {i: 0 for i in range(50)}
+
+        if ewc_lambda != 0:
+            plugins.append(SynapticIntelligencePlugin(ewc_lambda))
+
+        optimizer = SGD(model.parameters(), lr=lr, momentum=momentum,
+                        weight_decay=l2)
+        if criterion is None:
+            criterion = CrossEntropyLoss()
+
+        self.ewc_lambda = ewc_lambda
+        self.freeze_below_layer = freeze_below_layer
+        self.rm_sz = rm_sz
+        self.inc_update_rate = inc_update_rate
+        self.max_r_max = max_r_max
+        self.max_d_max = max_d_max
+        self.lr = lr
+        self.momentum = momentum
+        self.l2 = l2
+        self.rm = None
+
+        super().__init__(
+            model, optimizer, criterion,
+            train_mb_size=train_mb_size, train_epochs=train_epochs,
+            test_mb_size=test_mb_size, device=device, plugins=plugins)
+
+        # TODO: implement callbacks
 
 
 __all__ = ['Naive', 'CWRStar', 'Replay', 'GDumb', 'Cumulative', 'LwF', 'AGEM',
