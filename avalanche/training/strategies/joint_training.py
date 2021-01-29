72a7b6a192d247c72e1ce90d2040918add49dffb
AndreaCossu <andrea.cossu@sns.it>
2021-01-29 10:32:16 +0100

    Restored test step id for loggin purposes

--- avalanche/training/strategies/joint_training.py
+++ avalanche/training/strategies/joint_training.py
@@ -65,6 +65,7 @@ class JointTraining:
         self.task_layers = {}
 
         # Flow state variables
+        self.test_step_id = None  # test-flow only.
         self.epoch = None
         self.step_info = None  # we need to keep this for the eval plugin
         self.current_data = None
@@ -300,6 +301,7 @@ class JointTraining:
         for step_info in step_list:
             self.test_task_label = step_info.task_label
             self.step_info = step_info
+            self.test_step_id = step_info.current_step
 
             self.current_data = step_info.dataset
             self.adapt_test_dataset(**kwargs)
@@ -398,6 +400,7 @@ class JointTraining:
         for p in self.plugins:
             p.after_training_step(self, **kwargs)
         # Reset flow-state variables. They should not be used outside the flow
+        self.test_step_id = None
         self.epoch = None
         self.step_info = None
         self.current_data = None
