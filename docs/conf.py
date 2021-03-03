04e3296706a9f507da8cfe2be624abf58b75438b
Gabriele Graffieti <gabriele.graffieti@unibo.it>
2021-03-03 23:00:40 +0100

    add logo

--- docs/conf.py
+++ docs/conf.py
@@ -109,6 +109,7 @@ autoapi_keep_files = True
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_logo = './_static/img/avalanche_logo.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
