84598f18808ba74824bd89ea6fa76b42554c0196
Lorenzo Pellegrini <lrzpellegrini@gmail.com>
2021-03-12 11:45:19 +0100

    Fixed PEP8 issue

--- avalanche/__init__.py
+++ avalanche/__init__.py
@@ -7,10 +7,14 @@ def _adapt_dataset_urls():
     from torchvision.datasets import MNIST
     prev_mnist_urls = MNIST.resources
     new_resources = [
-        ('https://storage.googleapis.com/cvdf-datasets/mnist/train-images-idx3-ubyte.gz', prev_mnist_urls[0][1]),
-        ('https://storage.googleapis.com/cvdf-datasets/mnist/train-labels-idx1-ubyte.gz', prev_mnist_urls[1][1]),
-        ('https://storage.googleapis.com/cvdf-datasets/mnist/t10k-images-idx3-ubyte.gz', prev_mnist_urls[2][1]),
-        ('https://storage.googleapis.com/cvdf-datasets/mnist/t10k-labels-idx1-ubyte.gz', prev_mnist_urls[3][1])
+        ('https://storage.googleapis.com/cvdf-datasets/mnist/'
+         'train-images-idx3-ubyte.gz', prev_mnist_urls[0][1]),
+        ('https://storage.googleapis.com/cvdf-datasets/mnist/'
+         'train-labels-idx1-ubyte.gz', prev_mnist_urls[1][1]),
+        ('https://storage.googleapis.com/cvdf-datasets/mnist/'
+         't10k-images-idx3-ubyte.gz', prev_mnist_urls[2][1]),
+        ('https://storage.googleapis.com/cvdf-datasets/mnist/'
+         't10k-labels-idx1-ubyte.gz', prev_mnist_urls[3][1])
     ]
     MNIST.resources = new_resources
 
