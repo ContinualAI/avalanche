7cddaeb91bb609b07a4a88ebad143239f67ee5da
Gabriele Graffieti <gabriele.graffieti@unibo.it>
2021-03-29 20:28:02 +0200

    remove custom remote mirrors (use the default ones of torchvision) -- solve issue with the new torchvision version (0.9.1)

--- tests/unit_tests_utils.py
+++ tests/unit_tests_utils.py
@@ -2,18 +2,20 @@ from torchvision.datasets import MNIST
 
 
 def adapt_dataset_urls():
-    prev_mnist_urls = MNIST.resources
-    new_resources = [
-        ('https://storage.googleapis.com/cvdf-datasets/mnist/train-images-idx3-ubyte.gz', prev_mnist_urls[0][1]),
-        ('https://storage.googleapis.com/cvdf-datasets/mnist/train-labels-idx1-ubyte.gz', prev_mnist_urls[1][1]),
-        ('https://storage.googleapis.com/cvdf-datasets/mnist/t10k-images-idx3-ubyte.gz', prev_mnist_urls[2][1]),
-        ('https://storage.googleapis.com/cvdf-datasets/mnist/t10k-labels-idx1-ubyte.gz', prev_mnist_urls[3][1])
-    ]
-    MNIST.resources = new_resources
+    # prev_mnist_urls = MNIST.resources
+    # new_resources = [
+    #     ('https://storage.googleapis.com/cvdf-datasets/mnist/train-images-idx3-ubyte.gz', prev_mnist_urls[0][1]),
+    #     ('https://storage.googleapis.com/cvdf-datasets/mnist/train-labels-idx1-ubyte.gz', prev_mnist_urls[1][1]),
+    #     ('https://storage.googleapis.com/cvdf-datasets/mnist/t10k-images-idx3-ubyte.gz', prev_mnist_urls[2][1]),
+    #     ('https://storage.googleapis.com/cvdf-datasets/mnist/t10k-labels-idx1-ubyte.gz', prev_mnist_urls[3][1])
+    # ]
+    # MNIST.resources = new_resources
+    pass
 
 
 def common_setups():
-    adapt_dataset_urls()
+    # adapt_dataset_urls()
+    pass
 
 
 __all__ = [
