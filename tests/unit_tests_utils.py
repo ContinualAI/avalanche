30b701522f5bc7fd5db34169f9be345f35259a73
Gabriele Graffieti <gabriele.graffieti@unibo.it>
2021-03-29 19:22:41 +0200

    remove custom remote mirrors (use the default ones of torchvision)

--- tests/unit_tests_utils.py
+++ tests/unit_tests_utils.py
@@ -2,30 +2,32 @@ from torchvision.datasets import MNIST
 
 
 def adapt_dataset_urls():
-    prev_mnist_urls = MNIST.resources
+    # prev_mnist_urls = MNIST.resources
+    # # new_resources = [
+    # #     ('https://storage.googleapis.com/cvdf-datasets/mnist/train-images-idx3-ubyte.gz', prev_mnist_urls[0][1]),
+    # #     ('https://storage.googleapis.com/cvdf-datasets/mnist/train-labels-idx1-ubyte.gz', prev_mnist_urls[1][1]),
+    # #     ('https://storage.googleapis.com/cvdf-datasets/mnist/t10k-images-idx3-ubyte.gz', prev_mnist_urls[2][1]),
+    # #     ('https://storage.googleapis.com/cvdf-datasets/mnist/t10k-labels-idx1-ubyte.gz', prev_mnist_urls[3][1])
+    # # ]
+    #
     # new_resources = [
-    #     ('https://storage.googleapis.com/cvdf-datasets/mnist/train-images-idx3-ubyte.gz', prev_mnist_urls[0][1]),
-    #     ('https://storage.googleapis.com/cvdf-datasets/mnist/train-labels-idx1-ubyte.gz', prev_mnist_urls[1][1]),
-    #     ('https://storage.googleapis.com/cvdf-datasets/mnist/t10k-images-idx3-ubyte.gz', prev_mnist_urls[2][1]),
-    #     ('https://storage.googleapis.com/cvdf-datasets/mnist/t10k-labels-idx1-ubyte.gz', prev_mnist_urls[3][1])
+    #     ('https://github.com/mkolod/MNIST/raw/master/'
+    #      'train-images-idx3-ubyte.gz', prev_mnist_urls[0][1]),
+    #     ('https://github.com/mkolod/MNIST/raw/master/'
+    #      'train-labels-idx1-ubyte.gz', prev_mnist_urls[1][1]),
+    #     ('https://github.com/mkolod/MNIST/raw/master/'
+    #      't10k-images-idx3-ubyte.gz', prev_mnist_urls[2][1]),
+    #     ('https://github.com/mkolod/MNIST/raw/master/'
+    #      't10k-labels-idx1-ubyte.gz', prev_mnist_urls[3][1])
     # ]
-
-    new_resources = [
-        ('https://github.com/mkolod/MNIST/raw/master/'
-         'train-images-idx3-ubyte.gz', prev_mnist_urls[0][1]),
-        ('https://github.com/mkolod/MNIST/raw/master/'
-         'train-labels-idx1-ubyte.gz', prev_mnist_urls[1][1]),
-        ('https://github.com/mkolod/MNIST/raw/master/'
-         't10k-images-idx3-ubyte.gz', prev_mnist_urls[2][1]),
-        ('https://github.com/mkolod/MNIST/raw/master/'
-         't10k-labels-idx1-ubyte.gz', prev_mnist_urls[3][1])
-    ]
-
-    MNIST.resources = new_resources
+    #
+    # MNIST.resources = new_resources
+    pass
 
 
 def common_setups():
-    adapt_dataset_urls()
+    # adapt_dataset_urls()
+    pass
 
 
 __all__ = [
