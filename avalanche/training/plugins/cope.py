ef5be287efebe1e82ab542fa19829cec192fb02f
mattdl <de.lange.matthias@gmail.com>
2021-04-20 17:41:11 +0200

    CoPE using ClassBalancedStoragePolicy

--- avalanche/training/plugins/cope.py
+++ avalanche/training/plugins/cope.py
@@ -2,7 +2,8 @@ from typing import Dict
 import torch
 from torch.nn.functional import normalize
 
-from avalanche.benchmarks.utils import AvalancheConcatDataset
+from torch.utils.data.dataset import TensorDataset
+from avalanche.benchmarks.utils import AvalancheConcatDataset, AvalancheDataset
 from avalanche.training.plugins.strategy_plugin import StrategyPlugin
 from avalanche.training.plugins.replay import ClassBalancedStoragePolicy
 from avalanche.benchmarks.utils.data_loader import \
@@ -30,9 +31,10 @@ class CoPEPlugin(StrategyPlugin):
         # Operational memory: replay memory
         self.replay_mem = {}
         self.mem_size = mem_size  # replay memory size
-        self.storage_policy = ClassBalancedStoragePolicy(replay_mem=self.replay_mem,
-                                                         mem_size=self.mem_size,
-                                                         adaptive_size=True)
+        self.storage_policy = ClassBalancedStoragePolicy(
+            replay_mem=self.replay_mem,
+            mem_size=self.mem_size,
+            adaptive_size=True)
 
         # Operational memory: Prototypical memory
         self.p_mem = {}  # Scales with nb classes * feature size
@@ -63,7 +65,8 @@ class CoPEPlugin(StrategyPlugin):
             AvalancheConcatDataset(self.replay_mem.values()),
             oversample_small_tasks=False,
             num_workers=num_workers,
-            batch_size=strategy.train_mb_size * 2,  # bs = 10: 10 for new samples, 10 from replay
+            batch_size=strategy.train_mb_size * 2,
+            # bs = 10: 10 for new samples, 10 from replay
             shuffle=shuffle
         )
 
@@ -89,7 +92,8 @@ class CoPEPlugin(StrategyPlugin):
             c = y_unique[idx].item()
             if c not in self.p_mem:  # Init new prototype
                 self.p_mem[c] = normalize(
-                    torch.empty((1, self.p_size)).uniform_(0, 1), p=2, dim=1).detach().to(strategy.device)
+                    torch.empty((1, self.p_size)).uniform_(0, 1), p=2,
+                    dim=1).detach().to(strategy.device)
 
     @torch.no_grad()
     def _update_running_prototypes(self, strategy):
@@ -98,16 +102,21 @@ class CoPEPlugin(StrategyPlugin):
         for idx in range(y_unique.size(0)):
             c = y_unique[idx].item()
             idxs = torch.nonzero(strategy.mb_y == c).squeeze(1)
-            p_tmp_batch = strategy.logits[idxs].sum(dim=0).unsqueeze(0).to(strategy.device)
+            p_tmp_batch = strategy.logits[idxs].sum(dim=0).unsqueeze(0).to(
+                strategy.device)
 
-            p_init, cnt_init = self.tmp_p_mem[c] if c in self.tmp_p_mem else (0, 0)
+            p_init, cnt_init = self.tmp_p_mem[c] if c in self.tmp_p_mem else (
+                0, 0)
             self.tmp_p_mem[c] = (p_init + p_tmp_batch, cnt_init + len(idxs))
 
     def after_update(self, strategy, **kwargs):
         """ After the update on current batch, update prototypes and store samples.
         """
         self._update_prototypes()  # Update prototypes
-        self.storage_policy(strategy)  # Update memory
+
+        batch_data = AvalancheDataset(
+            TensorDataset(strategy.mb_x, strategy.mb_y))
+        self.storage_policy(batch_data)  # Update memory
 
     @torch.no_grad()
     def _update_prototypes(self):
@@ -115,8 +124,10 @@ class CoPEPlugin(StrategyPlugin):
         for c, (p_sum, p_cnt) in self.tmp_p_mem.items():
             incr_p = p_sum / p_cnt
             old_p = self.p_mem[c].clone()
-            new_p_momentum = self.alpha * old_p + (1 - self.alpha) * incr_p  # Momentum update
-            self.tmp_p_mem[c] = normalize(new_p_momentum, p=2, dim=1).detach()  # Update L2 normalized
+            new_p_momentum = self.alpha * old_p + (
+                    1 - self.alpha) * incr_p  # Momentum update
+            self.tmp_p_mem[c] = normalize(new_p_momentum, p=2,
+                                          dim=1).detach()  # Update L2 normalized
         self.tmp_p_mem = {}
 
 
@@ -143,22 +154,27 @@ class PPPloss(object):
         bs = x.size(0)
         x = x.view(bs, -1)  # Batch x feature size
         y_unique = torch.unique(y).squeeze().view(-1)
-        include_repellor = len(y_unique.size()) <= 1  # If only from the same class, there is no neg term
+        include_repellor = len(
+            y_unique.size()) <= 1  # If only from the same class, there is no neg term
 
         # All prototypes
         p_y = torch.tensor([c for c in self.p_mem.keys()]).to(x.device).detach()
-        p_x = torch.cat([self.p_mem[c.item()] for c in p_y]).to(x.device).detach()
+        p_x = torch.cat([self.p_mem[c.item()] for c in p_y]).to(
+            x.device).detach()
 
         for label_idx in range(y_unique.size(0)):  # Per-class operation
             c = y_unique[label_idx]
 
             # Make all-vs-rest batches per class
-            Bc = x.index_select(0, torch.nonzero(y == c).squeeze(dim=1))  # Attractor set (same class)
-            Bk = x.index_select(0, torch.nonzero(y != c).squeeze(dim=1))  # Repellor set (Other classes)
+            Bc = x.index_select(0, torch.nonzero(y == c).squeeze(
+                dim=1))  # Attractor set (same class)
+            Bk = x.index_select(0, torch.nonzero(y != c).squeeze(
+                dim=1))  # Repellor set (Other classes)
 
             p_idx = torch.nonzero(p_y == c).squeeze(dim=1)  # Prototypes
             pc = p_x[p_idx]  # Class prototype
-            pk = torch.cat([p_x[:p_idx], p_x[p_idx + 1:]])  # Other class prototypes
+            pk = torch.cat(
+                [p_x[:p_idx], p_x[p_idx + 1:]])  # Other class prototypes
 
             # Accumulate loss for instances of class c
             sum_logLc = self.attractor(pc, pk, Bc)
@@ -185,15 +201,19 @@ class PPPloss(object):
         :param Bc: Batch of instances of the same class c.
         :return: Sum_{i, the part of same class c} log P(c|x_i^c)
         """
-        m = torch.cat([Bc.clone(), pc, pk]).detach()  # Include all other-class prototypes p_k
+        m = torch.cat([Bc.clone(), pc,
+                       pk]).detach()  # Include all other-class prototypes p_k
         pk_idx = m.shape[0] - pk.shape[0]  # from when starts p_k
 
         # Resulting distance columns are per-instance loss terms (don't include self => diagonal)
-        D = torch.mm(m, Bc.t()).div_(self.T).exp_()  # Distance matrix in exp terms
-        mask = torch.eye(*D.shape).bool().to(Bc.device)  # Exclude product with self
+        D = torch.mm(m, Bc.t()).div_(
+            self.T).exp_()  # Distance matrix in exp terms
+        mask = torch.eye(*D.shape).bool().to(
+            Bc.device)  # Exclude product with self
         Dm = D.masked_fill(mask, 0)  # Masked out products with self
 
-        Lc_n, Lk_d = Dm[:pk_idx], Dm[pk_idx:].sum(dim=0)  # Numerator/denominator terms
+        Lc_n, Lk_d = Dm[:pk_idx], Dm[pk_idx:].sum(
+            dim=0)  # Numerator/denominator terms
         Pci = Lc_n / (Lc_n + Lk_d)  # Get probabilities per instance
         E_Pc = Pci.sum(0) / (Bc.shape[0])  # Expectation over pseudo-prototypes
         return E_Pc.log_().sum()  # sum over all instances (sum i)
@@ -217,6 +237,7 @@ class PPPloss(object):
         Lc_n = D[:pk_idx]
         Pki = Lc_n / (Lc_n + Lk_d)  # probability
 
-        E_Pk = (Pki[:-1] + Pki[-1].unsqueeze(0)) / 2  # Expectation pseudo/prototype
+        E_Pk = (Pki[:-1] + Pki[-1].unsqueeze(
+            0)) / 2  # Expectation pseudo/prototype
         inv_E_Pk = E_Pk.mul_(-1).add_(1).log_()  # log( (1 - Pk))
         return inv_E_Pk.sum()  # Sum over (pseudo-prototypes), and instances
