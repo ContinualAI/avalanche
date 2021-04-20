e038a821ae36f95f953e7f465efb543cb75f7d12
mattdl <de.lange.matthias@gmail.com>
2021-04-20 17:31:38 +0200

    ClassBalanced implemented, duplicate code

--- avalanche/training/plugins/replay.py
+++ avalanche/training/plugins/replay.py
@@ -3,7 +3,8 @@ from typing import Dict
 import random
 from torch.utils.data import random_split
 
-from avalanche.benchmarks.utils import AvalancheConcatDataset, AvalancheDataset
+from avalanche.benchmarks.utils import AvalancheConcatDataset, \
+    AvalancheDataset, AvalancheSubset
 from avalanche.training.plugins.strategy_plugin import StrategyPlugin
 from avalanche.benchmarks.utils.data_loader import \
     MultiTaskJoinedBatchDataLoader
@@ -170,52 +171,62 @@ class ClassBalancedStoragePolicy(StoragePolicy):
         self.mem_size = mem_size
         self.adaptive_size = adaptive_size
         self.total_num_classes = total_num_classes
+        self.seen_classes = set()
 
         if not self.adaptive_size:
             assert self.total_num_classes > 0, \
                 """When fixed exp mem size, total_num_classes should be > 0."""
 
-    def __call__(self, new_data, **kwargs):
-        curr_task_id = strategy.experience.task_label
-        curr_data = strategy.experience.dataset
+    def __call__(self, new_data: AvalancheDataset, **kwargs):
+
+        # Get class specific datasets
+        cl_datasets = {}
+        for idx, target in enumerate(new_data.targets):
+            if target not in cl_datasets:
+                cl_datasets[target] = []
+            cl_datasets[target].append(idx)
+
+        # Make AvalancheSubset per class
+        for c, c_idxs in cl_datasets.items():
+            cl_datasets[c] = AvalancheSubset(new_data, indices=c_idxs)
+
+        # Update seen classes
+        self.seen_classes.union(cl_datasets.keys())
 
         # how many experiences to divide the memory over
-        if self.adaptive_size:  # Observed number of experiences
-            exp_cnt = (strategy.training_exp_counter + 1)
-        else:  # Fixed number of experiences
-            exp_cnt = self.num_experiences
+        div_cnt = len(self.seen_classes) if self.adaptive_size \
+            else self.total_num_classes
+        class_mem_size = self.mem_size // div_cnt
 
-        exp_mem_size = self.mem_size // exp_cnt
-
-        # Add current experience data to memory
-        if curr_task_id not in self.ext_mem:
-            self.ext_mem[curr_task_id] = AvalancheDataset(curr_data)
-        else:  # Merge data with previous seen data
-            self.ext_mem[curr_task_id] = AvalancheConcatDataset(
-                [curr_data, self.ext_mem[curr_task_id]])
+        # Add current classes data to memory
+        for c in self.seen_classes:
+            if c not in self.ext_mem:
+                self.ext_mem[c] = cl_datasets[c]
+            else:  # Merge data with previous seen data
+                self.ext_mem[c] = AvalancheConcatDataset(
+                    [cl_datasets[c], self.ext_mem[c]])
 
         # Find number of remaining samples
-        samples_per_exp = {exp: len(m) for exp, m in self.ext_mem.items()}
-        remaining_from_exps = {exp: exp_mem_size - size for exp, size in
-                               samples_per_exp.items()
-                               if exp_mem_size - size > 0}
-        remaining_from_div = self.mem_size % (strategy.training_exp_counter + 1)
-        remaining_examples = sum(remaining_from_exps.values()) + \
-                             remaining_from_div
+        samples_per_cl = {c: len(m) for c, m in self.ext_mem.items()}
+        rem_from_cl = {exp: class_mem_size - size for exp, size in
+                             samples_per_cl.items()
+                             if class_mem_size - size > 0}
+        rem_from_div = self.mem_size % div_cnt
+        rem_examples = sum(rem_from_cl.values()) + rem_from_div
 
         # Divide the remaining samples randomly over the experiences
-        cutoff_per_exp = {exp: min(exp_mem_size, len(m))
+        cutoff_per_cl = {exp: min(class_mem_size, len(m))
                           for exp, m in self.ext_mem.items()}
-        while len(remaining_from_exps) > 0 and remaining_examples > 0:
-            exps_samples_left = list(remaining_from_exps.values())
+        while len(rem_from_cl) > 0 and rem_examples > 0:
+            exps_samples_left = list(rem_from_cl.values())
             exp = exps_samples_left[random.randrange(len(exps_samples_left))]
-            cutoff_per_exp[exp] += 1
-            remaining_examples -= 1
-            remaining_from_exps[exp] -= 1
-            if remaining_from_exps[exp] <= 0:
-                del remaining_from_exps[exp]
+            cutoff_per_cl[exp] += 1
+            rem_examples -= 1
+            rem_from_cl[exp] -= 1
+            if rem_from_cl[exp] <= 0:
+                del rem_from_cl[exp]
 
         # Allocate to experiences
-        for exp, cutoff in cutoff_per_exp.items():
+        for exp, cutoff in cutoff_per_cl.items():
             self.ext_mem[exp], _ = random_split(
                 self.ext_mem[exp], [cutoff, len(self.ext_mem[exp]) - cutoff])
