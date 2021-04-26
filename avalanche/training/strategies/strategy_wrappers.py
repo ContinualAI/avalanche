4d1c761feb01cbe32de6c9593401f54cf3d90d4c
BELOUADAH Eden <eden.belouadah@cea.fr>
2021-04-26 15:11:30 +0200

    fix hyperparameters and unittest bugs

--- avalanche/training/strategies/strategy_wrappers.py
+++ avalanche/training/strategies/strategy_wrappers.py
@@ -449,50 +449,6 @@ class SynapticIntelligence(BaseStrategy):
             eval_every=eval_every
         )
 
-    def __init__(self, model: Module, optimizer: Optimizer, criterion,
-                 patterns_per_exp: int, memory_strength: float = 0.5,
-                 train_mb_size: int = 1, train_epochs: int = 1,
-                 eval_mb_size: int = None, device=None,
-                 plugins: Optional[List[StrategyPlugin]] = None,
-                 evaluator: EvaluationPlugin = default_logger, eval_every=-1):
-        """ Gradient Episodic Memory (GEM) strategy.
-            See GEM plugin for details.
-            This strategy does not use task identities.
-
-        :param model: The model.
-        :param optimizer: The optimizer to use.
-        :param criterion: The loss criterion to use.
-        :param patterns_per_exp: number of patterns per experience in the memory
-        :param memory_strength: offset to add to the projection direction
-            in order to favour backward transfer (gamma in original paper).
-        :param train_mb_size: The train minibatch size. Defaults to 1.
-        :param train_epochs: The number of training epochs. Defaults to 1.
-        :param eval_mb_size: The eval minibatch size. Defaults to 1.
-        :param device: The device to use. Defaults to None (cpu).
-        :param plugins: Plugins to be added. Defaults to None.
-        :param evaluator: (optional) instance of EvaluationPlugin for logging
-            and metric computations.
-        :param eval_every: the frequency of the calls to `eval` inside the
-            training loop.
-                if -1: no evaluation during training.
-                if  0: calls `eval` after the final epoch of each training
-                    experience.
-                if >0: calls `eval` every `eval_every` epochs and at the end
-                    of all the epochs for a single experience.
-        """
-
-        gem = GEMPlugin(patterns_per_exp, memory_strength)
-        if plugins is None:
-            plugins = [gem]
-        else:
-            plugins.append(gem)
-
-        super().__init__(
-            model, optimizer, criterion,
-            train_mb_size=train_mb_size, train_epochs=train_epochs,
-            eval_mb_size=eval_mb_size, device=device, plugins=plugins,
-            evaluator=evaluator, eval_every=eval_every)
-
 
 class SIW(BaseStrategy):
     def __init__(self, model: Module, optimizer: Optimizer, criterion,
--- examples/siw_cifar100.py
+++ examples/siw_cifar100.py
@@ -1,37 +1,58 @@
 from avalanche.benchmarks.classic import SplitCIFAR100
 from torch.optim import SGD
 from torch.nn import CrossEntropyLoss
-from avalanche.models import SimpleMLP
 from avalanche.training.strategies import Naive
-from avalanche.training.plugins import ReplayPlugin, EWCPlugin, \
-    AGEMPlugin, SIWPlugin, EvaluationPlugin
-from avalanche.logging import InteractiveLogger, TextLogger
+from avalanche.training.plugins import SIWPlugin,\
+    EvaluationPlugin, StrategyPlugin
+from avalanche.logging import InteractiveLogger
 from avalanche.evaluation.metrics import accuracy_metrics
 import torchvision
-from avalanche.benchmarks.generators import filelist_scenario, \
-    dataset_scenario, tensor_scenario, paths_scenario
-from torchvision.transforms import Compose, CenterCrop, Normalize, \
-    Scale, Resize, ToTensor, ToPILImage
 import torchvision.transforms as transforms
 import torch.nn as nn
 import torch
-from torch.autograd import Variable
 import argparse
+from torch.optim import lr_scheduler
+
+
+class LRSchedulerPlugin(StrategyPlugin):
+    def __init__(self, lr_scheduler):
+        super().__init__()
+        self.lr_scheduler = lr_scheduler
+
+    def after_training_epoch(self, strategy: 'BaseStrategy', **kwargs):
+        self.lr_scheduler.step(strategy.loss.cpu().data.numpy())
+        lr = strategy.optimizer.param_groups[0]['lr']
+        print(f"\nlr = {lr}")
+
+
+class SetIncrementalHyperParams(StrategyPlugin):
+    def __init__(self, inc_exp_epochs, inc_exp_patience, first_exp_lr,
+                 lr_decay):
+        super().__init__()
+        self.inc_exp_epochs = inc_exp_epochs
+        self.inc_exp_patience = inc_exp_patience
+        self.first_exp_lr = first_exp_lr
+        self.lr_decay = lr_decay
+
+    def before_training_exp(self, strategy: 'BaseStrategy', **kwargs):
+        if strategy.experience.current_experience > 0:  # incremental update
+            strategy.train_epochs = self.inc_exp_epochs
+            strategy.optimizer.param_groups[0]['lr'] = \
+                self.first_exp_lr / strategy.experience.current_experience
+            strategy.scheduler = LRSchedulerPlugin(
+                lr_scheduler.ReduceLROnPlateau(strategy.optimizer,
+                                               patience=self.inc_exp_patience,
+                                               factor=self.lr_decay))
 
 
 def main(args):
     # check if selected GPU is available or use CPU
     assert args.cuda == -1 or args.cuda >= 0, "cuda must be -1 or >= 0."
-    device = torch.device(f"cuda:{args.cuda}"
-                          if torch.cuda.is_available()
+    device = torch.device(f"cuda:{args.cuda}" if torch.cuda.is_available()
                           and args.cuda >= 0 else "cpu")
     print(f'Using device: {device}')
     #############################################
     model = torchvision.models.resnet18(num_classes=100).to(device)
-
-    siw = SIWPlugin(model, siw_layer_name=args.siw_layer_name,
-                    batch_size=args.siw_batch_size,
-                    num_workers=args.siw_num_workers)
 
     # print to stdout
     interactive_logger = InteractiveLogger()
@@ -42,14 +63,32 @@ def main(args):
         loggers=[interactive_logger]
     )
 
-    optimizer = SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    optimizer = SGD(model.parameters(), lr=args.first_exp_lr,
+                    momentum=args.momentum,
+                    weight_decay=args.weight_decay)
     criterion = CrossEntropyLoss()
-    strategy = Naive(model, optimizer, criterion, plugins=[siw],
-                     device=device, train_epochs=args.epochs,
-                     evaluator=eval_plugin)
+    scheduler = LRSchedulerPlugin(
+        lr_scheduler.ReduceLROnPlateau(optimizer,
+                                       patience=args.first_exp_patience,
+                                       factor=args.lr_decay))
+    incremental_params = SetIncrementalHyperParams(args.inc_exp_epochs,
+                                                   args.inc_exp_patience,
+                                                   args.first_exp_lr,
+                                                   args.lr_decay)
 
-    normalize = transforms.Normalize(mean=[0.5356, 0.4898, 0.4255],
-                                     std=[0.2007, 0.1999, 0.1992])
+    siw = SIWPlugin(model, siw_layer_name=args.siw_layer_name,
+                    batch_size=args.eval_batch_size,
+                    num_workers=args.num_workers)
+
+    strategy = Naive(model, optimizer, criterion,
+                     device=device, train_epochs=args.first_exp_epochs,
+                     evaluator=eval_plugin,
+                     plugins=[siw, scheduler, incremental_params],
+                     train_mb_size=args.train_batch_size,
+                     eval_mb_size=args.eval_batch_size)
+
+    normalize = transforms.Normalize(mean=[0.5071, 0.4866, 0.4409],
+                                     std=[0.2673, 0.2564, 0.2762])
 
     train_transform = transforms.Compose([
         transforms.RandomResizedCrop(224),
@@ -68,16 +107,16 @@ def main(args):
                              fixed_class_order=range(0, 100),
                              train_transform=train_transform,
                              eval_transform=test_transform)
-
     # TRAINING LOOP
     print('Starting experiment...')
     results = []
     for i, experience in enumerate(scenario.train_stream):
         print("Start of experience: ", experience.current_experience)
-        strategy.train(experience)
+        strategy.train(experience, num_workers=args.num_workers)
         print('Training completed')
         print('Computing accuracy on the test set')
-        res = strategy.eval(scenario.test_stream[:i+1])
+        res = strategy.eval(scenario.test_stream[:i + 1],
+                            num_workers=args.num_workers)
         results.append(res)
 
     print('Results = ' + str(results))
@@ -85,15 +124,27 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--lr', type=float, default=0.1, help='Learning rate.')
-    parser.add_argument('--momentum', type=float, default=0.9, help='Momentum')
-    parser.add_argument('--epochs', type=int, default=100,
-                        help='Number of training epochs.')
-    parser.add_argument('--batch_size', type=int, default=128,
-                        help='Batch size.')
-    parser.add_argument('--siw_batch_size', type=int, default=128,
-                        help='Batch size used to extract scores.')
-    parser.add_argument('--siw_num_workers', type=int, default=8,
+    parser.add_argument('--first_exp_lr', type=float, default=0.1,
+                        help='Learning rate for the first experience.')
+    parser.add_argument('--momentum', type=float, default=0.9,
+                        help='Momentum')
+    parser.add_argument('--weight_decay', type=float, default=0.0005,
+                        help='Weight decay')
+    parser.add_argument('--lr_decay', type=float, default=0.1,
+                        help='LR decay')
+    parser.add_argument('--first_exp_patience', type=int, default=60,
+                        help='Patience in the first experience')
+    parser.add_argument('--inc_exp_patience', type=int, default=15,
+                        help='Patience in the incremental experiences')
+    parser.add_argument('--first_exp_epochs', type=int, default=300,
+                        help='Number of epochs in the first experience.')
+    parser.add_argument('--inc_exp_epochs', type=int, default=70,
+                        help='Number of epochs in each incremental experience.')
+    parser.add_argument('--train_batch_size', type=int, default=128,
+                        help='Training batch size.')
+    parser.add_argument('--eval_batch_size', type=int, default=32,
+                        help='Evaluation batch size.')
+    parser.add_argument('--num_workers', type=int, default=8,
                         help='Number of workers used to extract scores.')
     parser.add_argument('--siw_layer_name', type=str, default='fc',
                         help='Name of the last fully connected layer.')
