ae65f1410f6ca541aec6a416c3ff9f9f3f0a5663
Tom <veniat.tom@gmail.com>
2021-05-04 12:39:41 +0200

    Add tests

--- avalanche/benchmarks/classic/ctrl.py
+++ avalanche/benchmarks/classic/ctrl.py
@@ -1,13 +1,20 @@
-import torch
-from avalanche.benchmarks import GenericCLScenario, dataset_benchmark
-from avalanche.benchmarks.utils import AvalancheTensorDataset
 from torchvision import transforms
 
 import ctrl
+from avalanche.benchmarks import dataset_benchmark
+from avalanche.benchmarks.utils import AvalancheTensorDataset
 
 
-def CTrL(stream_name):
-    stream = ctrl.get_stream(stream_name)
+def CTrL(stream_name: str, seed: int = None):
+    """
+    Gives access to the Continual Transfer Learning benchmark streams
+    introduced in https://arxiv.org/abs/2012.12631.
+    :param stream_name: Name of the test stream to generate. Must be one of
+    `s_plus`, `s_minus`, `s_in`, `s_out` and `s_pl`.
+    :param seed: The seed to use to generate the streams.
+    :return: A scenario containing 3 streams: train, val and test.
+    """
+    stream = ctrl.get_stream(stream_name, seed)
 
     # Train, val and test experiences
     exps = [[], [], []]
@@ -21,6 +28,7 @@ def CTrL(stream_name):
                                              task_labels=task_labels,
                                              transform=trans)
             exp.append(dataset)
+
     return dataset_benchmark(
         train_datasets=exps[0],
         test_datasets=exps[2],
