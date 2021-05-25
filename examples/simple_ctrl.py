1ca7ccac8c23c5e6633effed80130b242c1a6a8a
Tom <veniat.tom@gmail.com>
2021-05-25 12:38:11 +0200

    Clone model to compute transfer

--- examples/simple_ctrl.py
+++ examples/simple_ctrl.py
@@ -18,6 +18,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from copy import deepcopy
+from pprint import pprint
+
 import torch
 import argparse
 
@@ -28,7 +31,10 @@ from avalanche.benchmarks import GenericCLScenario
 from avalanche.benchmarks.classic import PermutedMNIST, RotatedMNIST, \
     SplitMNIST
 from avalanche.benchmarks.classic.ctrl import CTrL
+from avalanche.evaluation.metrics import accuracy_metrics, loss_metrics
+from avalanche.logging import InteractiveLogger
 from avalanche.models import SimpleMLP, SimpleCNN
+from avalanche.training import EvaluationPlugin
 from avalanche.training.strategies import Naive
 
 def main(args):
@@ -40,6 +46,7 @@ def main(args):
     # model
     # model = SimpleMLP(num_classes=11, input_size=3*32*32)
     model = SimpleCNN(num_classes=10)
+    model_last = deepcopy(model)
 
     scenario = CTrL(stream_name='s_plus')
 
@@ -52,25 +59,47 @@ def main(args):
     criterion = CrossEntropyLoss()
 
     # Continual learning strategy with default logger
+
+    logger = EvaluationPlugin(
+        accuracy_metrics(minibatch=False, epoch=True, experience=True,
+                         stream=True),
+        loss_metrics(minibatch=False, epoch=True, experience=True,
+                     stream=True),
+    loggers=[InteractiveLogger()])
     cl_strategy = Naive(
         model, optimizer, criterion, train_mb_size=32, train_epochs=200,
-        eval_mb_size=32, device=device)
+        eval_mb_size=128, device=device, evaluator=logger)
 
     # train and test loop
-    results = []
-    transfer_mat = []
-    for train_task in train_stream:
-        print("Current Classes: ", train_task.classes_in_this_experience)
-        res = cl_strategy.train(train_task)
-        print('')
-        print(f'Train res: {res}')
-        results.append(cl_strategy.eval(test_stream)) 
-        print(f'Val res: {results}')
-        
-        transfer_mat.append([results[-1][f'Top1_Acc_Exp/eval_phase/test_stream/Task00{i}/Exp00{i}'] for i in range(len(train_stream))])
-        print(torch.tensor(transfer_mat))
-        print((torch.tensor(transfer_mat)*1000).round()/1000)
+    assert logger == cl_strategy.evaluator
+    for tid, train_task in enumerate(train_stream):
+        cl_strategy.train(train_task)
+        cl_strategy.eval(test_stream)
 
+
+    # print()
+
+    transfer_mat = []
+    for tid in range(len(train_stream)):
+        transfer_mat.append(
+            logger.all_metric_results[f'Top1_Acc_Exp/eval_phase/test_stream/Task00{tid}/Exp00{tid}'][1])
+    # cl_strategy.evaluator.all_metric_results[
+    #     f'Top1_Acc_Exp/eval_phase/test_stream/Task00{tid}/Exp00{tid}'][1])
+
+    print(torch.tensor(transfer_mat))
+    # pprint(logger.all_metric_results)
+    pprint(logger.last_metric_results)
+
+    optimizer = SGD(model_last.parameters(), lr=0.001, momentum=0.9)
+    cl_strategy = Naive(
+        model_last, optimizer, criterion, train_mb_size=32, train_epochs=200,
+        eval_mb_size=32, device=device)
+    cl_strategy.train(train_task)
+    res = cl_strategy.eval([test_stream[-1]])
+    print()
+    print(res)
+
+    print(transfer_mat[-1][-1] - res['Top1_Acc_Exp/eval_phase/test_stream/Task005/Exp-01'])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
