f4d50a03357f62f47e5453cea9aa217168e4673b
Xa9aX ツ <mishradiganta91@gmail.com>
2021-06-18 06:28:27 +0530

    Updated WandB hook

--- avalanche/logging/wandb_logger.py
+++ avalanche/logging/wandb_logger.py
@@ -4,22 +4,22 @@
 # See the accompanying LICENSE file for terms.                                 #
 #                                                                              #
 # Date: 25-11-2020                                                             #
-# Author(s): Vincenzo Lomonaco, Lorenzo Pellegrini                             #
+# Author(s): Diganta Misra, Andrea Cossu                                       #
 # E-mail: contact@continualai.org                                              #
 # Website: www.continualai.org                                                 #
 ################################################################################
-
 """ This module handles all the functionalities related to the logging of
 Avalanche experiments using Weights & Biases. """
 from PIL.Image import Image
 from numpy import array
 from torch import Tensor
+import torch
 from matplotlib.pyplot import Figure
-
 from avalanche.evaluation.metric_results import AlternativeValues, \
     MetricValue, TensorImage
 from avalanche.logging import StrategyLogger
 import numpy as np
+import os
 
 
 class WandBLogger(StrategyLogger):
@@ -30,22 +30,30 @@ class WandBLogger(StrategyLogger):
     """
 
     def __init__(self, project_name: str = "Avalanche", 
-                 run_name: str = "Avalanche Test", params: dict = None):
+                 run_name: str = "Test", log_artifacts: bool = False,
+                 path: str = "Checkpoints", checkpoint: str = "Model.h5", 
+                 uri: str = None, params: dict = None):
         """
         Creates an instance of the `WandBLogger`.
-
         :param project_name: Name of the W&B project.:
         :param run_name: Name of the W&B run.:
+        :param log_artifacts: Option to log model weights as W&B Artifacts.:
+        :param path: Path to locally save the model checkpoints.:
+        :param checkpoint: Name of the model checkpoint file.:
+        :param uri: Reference to external URI.:
         :param params: All arguments for wandb.init() function call. 
          Visit https://docs.wandb.ai/ref/python/init to learn about all 
          wand.init() parameters.:
         """
-
         super().__init__()
         self.import_wandb()
-        self.params = params
         self.project_name = project_name
         self.run_name = run_name
+        self.log_artifacts = log_artifacts
+        self.path = path
+        self.checkpoint = checkpoint
+        self.uri = uri
+        self.params = params
         self.args_parse()
         self.before_run()
 
@@ -74,30 +82,47 @@ class WandBLogger(StrategyLogger):
         super().log_metric(metric_value, callback)
         name = metric_value.name
         value = metric_value.value
-
         if isinstance(value, AlternativeValues):
             value = value.best_supported_value(Image, Tensor, TensorImage,
                                                Figure, float, int,
                                                self.wandb.viz.CustomChart)
-
         if not isinstance(value, (Image, Tensor, Figure, float, int,
                                   self.wandb.viz.CustomChart)):
             # Unsupported type
             return
-
         if isinstance(value, Image):
             self.wandb.log({name: self.wandb.Image(value)})
-
         elif isinstance(value, Tensor):
             value = np.histogram(value.view(-1).numpy())
             self.wandb.log({name: self.wandb.Histogram(np_histogram=value)})
-
         elif isinstance(value, (float, int, Figure,
                                 self.wandb.viz.CustomChart)):
             self.wandb.log({name: value})
 
-        elif isinstance(value, TensorImage):
-            self.wandb.log({name: self.wandb.Image(array(value))})
+        elif isinstance(value, TensorImage):	
+            self.wandb.log({name: self.wandb.Image(array(value))})	
+
+    def after_training_exp(self, weights: MetricValue, callback: str):
+        super().after_training_exp(weights, callback)
+        value = weights.value
+        if self.log_artifacts: 
+            cwd = os.getcwd()
+            ckpt = os.path.join(cwd, self.path)
+            try:
+                os.makedirs(ckpt)
+            except OSError as e:
+                if e.errno != errno.EEXIST:
+                    raise
+            dir_name = os.path.join(ckpt, self.checkpoint)
+            artifact_name = os.path.join('Models', self.checkpoint)
+            if isinstance(value, list):
+                torch.save(value, dir_name)
+                name = os.path.splittext(self.checkpoint)
+                artifact = self.wandb.Artifact(name, type='model')
+                artifact.add_file(dir_name, name=artifact_name)
+                self.wandb.run.log_artifact(artifact)
+                if self.uri is not None:
+                    artifact.add_reference(self.uri, name=artifact_name)
 
 
 __all__ = [
