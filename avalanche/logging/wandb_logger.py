7fc697c52980c8af4cacb628ff2cd8a4d7cb504b
Xa9aX ツ <mishradiganta91@gmail.com>
2021-07-15 22:17:31 +0530

    Added WeightCheckpoint Metric

--- avalanche/logging/wandb_logger.py
+++ avalanche/logging/wandb_logger.py
@@ -20,6 +20,7 @@ import numpy as np
 from numpy import array
 from torch import Tensor
 import torch
+from torch import Tensor
 
 from PIL.Image import Image
 from matplotlib.pyplot import Figure
@@ -46,8 +47,7 @@ class WandBLogger(StrategyLogger):
 
     def __init__(self, project_name: str = "Avalanche", 
                  run_name: str = "Test", log_artifacts: bool = False,
-                 path: Union[str, Path] = "/Checkpoints", 
-                 checkpoint: str = "Model.pth", 
+                 path: Union[str, Path] = "Checkpoints", 
                  uri: str = None, sync_tfboard: bool = False, 
                  save_code: bool = True, config: object = None, 
                  dir: Union[str, Path] = None, params: dict = None):
@@ -57,7 +57,6 @@ class WandBLogger(StrategyLogger):
         :param run_name: Name of the W&B run.
         :param log_artifacts: Option to log model weights as W&B Artifacts.
         :param path: Path to locally save the model checkpoints.
-        :param checkpoint: Name of the model checkpoint file.
         :param uri: Reference to external URI.
         :param sync_tfboard: Syncs TensorBoard to the W&B dashboard UI.
         :param save_code: Saves the main training script to W&B. 
@@ -73,7 +72,6 @@ class WandBLogger(StrategyLogger):
         self.run_name = run_name
         self.log_artifacts = log_artifacts
         self.path = path
-        self.checkpoint = checkpoint
         self.uri = uri
         self.sync_tfboard = sync_tfboard
         self.save_code = save_code
@@ -112,47 +110,51 @@ class WandBLogger(StrategyLogger):
         super().log_metric(metric_value, callback)
         name = metric_value.name
         value = metric_value.value
+
         if isinstance(value, AlternativeValues):
             value = value.best_supported_value(Image, Tensor, TensorImage,
                                                Figure, float, int,
                                                self.wandb.viz.CustomChart)
+
         if not isinstance(value, (Image, Tensor, Figure, float, int,
                                   self.wandb.viz.CustomChart)):
             # Unsupported type
             return
+        
         if isinstance(value, Image):
             self.wandb.log({name: self.wandb.Image(value)})
+        
         elif isinstance(value, Tensor):
             value = np.histogram(value.view(-1).numpy())
             self.wandb.log({name: self.wandb.Histogram(np_histogram=value)})
+        
         elif isinstance(value, (float, int, Figure,
                                 self.wandb.viz.CustomChart)):
             self.wandb.log({name: value})
 
         elif isinstance(value, TensorImage):	
-            self.wandb.log({name: self.wandb.Image(array(value))})	
+            self.wandb.log({name: self.wandb.Image(array(value))})
 
-    def after_training_exp(self, weights: MetricValue, callback: str):
-        super().after_training_exp(weights, callback)
-        value = weights.value
-        if self.log_artifacts: 
-            cwd = os.getcwd()
-            ckpt = os.path.join(cwd, self.path)
-            try:
-                os.makedirs(ckpt)
-            except OSError as e:
-                if e.errno != errno.EEXIST:
-                    raise
-            dir_name = os.path.join(ckpt, self.checkpoint)
-            artifact_name = os.path.join('Models', self.checkpoint)
-            if isinstance(value, list):
-                torch.save(value, dir_name)
-                name = os.path.splittext(self.checkpoint)
-                artifact = self.wandb.Artifact(name, type='model')
-                artifact.add_file(dir_name, name=artifact_name)
-                self.wandb.run.log_artifact(artifact)
-                if self.uri is not None:
-                    artifact.add_reference(self.uri, name=artifact_name)
+        elif name.startswith("WeightCheckpoint"):
+            if self.log_artifacts:
+                cwd = os.getcwd()
+                ckpt = os.path.join(cwd, self.path)
+                try:
+                    os.makedirs(ckpt)
+                except OSError as e:
+                    if e.errno != errno.EEXIST:
+                        raise
+                suffix = '.pth'
+                dir_name = os.path.join(ckpt, name+suffix)
+                artifact_name = os.path.join('Models', name+suffix)
+                if isinstance(value, Tensor):
+                    torch.save(value, dir_name)
+                    name = os.path.splittext(self.checkpoint)
+                    artifact = self.wandb.Artifact(name, type='model')
+                    artifact.add_file(dir_name, name=artifact_name)
+                    self.wandb.run.log_artifact(artifact)
+                    if self.uri is not None:
+                        artifact.add_reference(self.uri, name=artifact_name)
 
 
 __all__ = [
