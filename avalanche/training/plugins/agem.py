3cf9dde58224759d63a18351cc9c89f7c7500b5a
Antonio Carta <antonio.carta@di.unipi.it>
2021-07-23 16:40:36 +0200

    ADD task label support for A-GEM

--- avalanche/training/plugins/agem.py
+++ avalanche/training/plugins/agem.py
@@ -1,7 +1,12 @@
 import random
 
 import torch
+from torch.utils.data import random_split, DataLoader
 
+from avalanche.benchmarks.utils import AvalancheConcatDataset
+from avalanche.benchmarks.utils.data_loader import GroupBalancedDataLoader, \
+    GroupBalancedInfiniteDataLoader
+from avalanche.models import avalanche_forward
 from avalanche.training.plugins.strategy_plugin import StrategyPlugin
 
 
@@ -28,6 +33,10 @@ class AGEMPlugin(StrategyPlugin):
         self.patterns_per_experience = int(patterns_per_experience)
         self.sample_size = int(sample_size)
 
+        self.buffers = []  # one AvalancheDataset for each experience.
+        self.buffer_dataloader = None
+        self.buffer_dliter = None
+
         self.reference_gradients = None
         self.memory_x, self.memory_y = None, None
 
@@ -35,94 +44,75 @@ class AGEMPlugin(StrategyPlugin):
         """
         Compute reference gradient on memory sample.
         """
-
-        if self.memory_x is not None:
+        if len(self.buffers) > 0:
             strategy.model.train()
             strategy.optimizer.zero_grad()
-            xref, yref = self.sample_from_memory(self.sample_size)
+            mb = self.sample_from_memory()
+            xref, yref, tid = mb[0], mb[1], mb[-1]
             xref, yref = xref.to(strategy.device), yref.to(strategy.device)
-            out = strategy.model(xref)
+
+            out = avalanche_forward(strategy.model, xref, tid)
             loss = strategy._criterion(out, yref)
             loss.backward()
             self.reference_gradients = [
-                (n, p.grad)
-                for n, p in strategy.model.named_parameters()]
+                p.grad.view(-1) for n, p
+                in strategy.model.named_parameters() if p.requires_grad]
+            self.reference_gradients = torch.cat(self.reference_gradients)
+            strategy.optimizer.zero_grad()
 
     @torch.no_grad()
     def after_backward(self, strategy, **kwargs):
         """
         Project gradient based on reference gradients
         """
-        if self.memory_x is not None:
+        if len(self.buffers) > 0:
             current_gradients = [p.grad.view(-1)
-                for n, p in strategy.model.named_parameters() if p.requires_grad]
+                for n, p in strategy.model.named_parameters()
+                                 if p.requires_grad]
             current_gradients = torch.cat(current_gradients)
 
-            assert current_gradients.shape == self.reference_gradients.shape , "Different model parameters in AGEM projection"
+            assert current_gradients.shape == self.reference_gradients.shape, \
+                "Different model parameters in AGEM projection"
 
-            dotg = torch.dot( current_gradients, self.reference_gradients)
+            dotg = torch.dot(current_gradients, self.reference_gradients)
             if dotg < 0:
-                alpha2 = dotg / torch.dot(self.reference_gradients, self.reference_gradients)
-                grad_proj = current_gradients - self.reference_gradients * alpha2
+                alpha2 = dotg / torch.dot(self.reference_gradients,
+                                          self.reference_gradients)
+                grad_proj = current_gradients - \
+                            self.reference_gradients * alpha2
                 
                 count = 0 
                 for n, p in strategy.model.named_parameters():
                     if p.requires_grad:
                         n_param = p.numel()      
-                        p.grad.copy_( grad_proj[count:count+n_param].view_as(p) )
+                        p.grad.copy_(grad_proj[count:count+n_param].view_as(p))
                         count += n_param
 
     def after_training_exp(self, strategy, **kwargs):
-        """
-        Save a copy of the model after each experience
-        """
+        """ Update replay memory with patterns from current experience. """
+        self.update_memory(strategy.experience.dataset)
 
-        self.update_memory(strategy.dataloader)
-
-    def sample_from_memory(self, sample_size):
+    def sample_from_memory(self):
         """
         Sample a minibatch from memory.
         Return a tuple of patterns (tensor), targets (tensor).
         """
-
-        if self.memory_x is None or self.memory_y is None:
-            raise ValueError('Empty memory for AGEM.')
-
-        if self.memory_x.size(0) <= sample_size:
-            return self.memory_x, self.memory_y
-        else:
-            idxs = random.sample(range(self.memory_x.size(0)), sample_size)
-            return self.memory_x[idxs], self.memory_y[idxs]
+        return next(self.buffer_dliter)
 
     @torch.no_grad()
-    def update_memory(self, dataloader):
+    def update_memory(self, dataset):
         """
         Update replay memory with patterns from current experience.
         """
-        tot = 0
-        done = False
-        for batches in dataloader:
-            for _, (x, y) in batches.items():
-                if tot + x.size(0) <= self.patterns_per_experience:
-                    if self.memory_x is None:
-                        self.memory_x = x.clone()
-                        self.memory_y = y.clone()
-                    else:
-                        self.memory_x = torch.cat((self.memory_x, x), dim=0)
-                        self.memory_y = torch.cat((self.memory_y, y), dim=0)
-                    tot += x.size(0)
-                else:
-                    diff = self.patterns_per_experience - tot
-                    if self.memory_x is None:
-                        self.memory_x = x[:diff].clone()
-                        self.memory_y = y[:diff].clone()
-                    else:
-                        self.memory_x = torch.cat((self.memory_x,
-                                                   x[:diff]), dim=0)
-                        self.memory_y = torch.cat((self.memory_y,
-                                                   y[:diff]), dim=0)
-                    tot += diff
-                    done = True
-                    
-                if done: break
-            if done: break
+        removed_els = len(dataset) - self.patterns_per_experience
+        if removed_els > 0:
+            dataset, _ = random_split(dataset,
+                                      [self.patterns_per_experience,
+                                       removed_els])
+        self.buffers.append(dataset)
+        self.buffer_dataloader = GroupBalancedInfiniteDataLoader(self.buffers,
+                                            batch_size=self.sample_size // len(self.buffers),
+                                            num_workers=4,
+                                            pin_memory=True,
+                                            persistent_workers=True)
+        self.buffer_dliter = iter(self.buffer_dataloader)
