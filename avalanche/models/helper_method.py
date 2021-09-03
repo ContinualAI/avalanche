7af366ea1341843a326a37f0a4be9a85da80a040
AlbinSou <albin.soutif@gmail.com>
2021-09-03 13:32:42 +0200

    Added initialization to old param values and tests
    changed __init__ file to allow as_multitask import.
    Added parameter initialization to single task classifier
    parameters. Added output and parameters comparison test.

--- avalanche/models/helper_method.py
+++ avalanche/models/helper_method.py
@@ -2,13 +2,15 @@
 import torch
 import torch.nn as nn
 
-from avalanche.models.dynamic_modules import MultiTaskModule, MultiHeadClassifier
+from avalanche.models.dynamic_modules import MultiTaskModule, \
+MultiHeadClassifier
+
 
 class MultiTaskDecorator(MultiTaskModule):
     """ 
     Encapsulates an existing nn.Module to make it subclass MultiTaskModule,
-    the user should still be able to interact with the encapsulated module as if
-    it was the module itself. 
+    the user should still be able to interact with the encapsulated module 
+    as if it was the module itself. 
     
     The only things that change are the following, the classifier from the
     given model will be replaced by a MultiHeadClassifier, and the forward()
@@ -32,18 +34,26 @@ class MultiTaskDecorator(MultiTaskModule):
         if isinstance(old_classifier, nn.Linear):
             in_size = old_classifier.in_features 
             out_size = old_classifier.out_features 
+            old_params = [torch.clone(p.data) for p in \
+            old_classifier.parameters()]
             # Replace old classifier by empty block
             setattr(self.model, classifier_name, nn.Sequential())
         elif isinstance(old_classifier, nn.Sequential):
             in_size = old_classifier[-1].in_features 
             out_size = old_classifier[-1].out_features 
+            old_params = [torch.clone(p.data) for p in \
+            old_classifier[-1].parameters()]
             del old_classifier[-1]
         else:
             raise NotImplementedError(f"Cannot handle the following type \
             of classification layer {type(old_classifier)}")
 
-        # Set new classifier
+        # Set new classifier and initialize to previous param values
         setattr(self, classifier_name, MultiHeadClassifier(in_size, out_size))
+
+        for param, param_old in \
+        zip(getattr(self, classifier_name).parameters(), old_params):
+            param.data = param_old
 
         self._initialized = True
 
@@ -74,19 +84,21 @@ class MultiTaskDecorator(MultiTaskModule):
         else:
             return setattr(self.model, name, value) 
 
+
 def as_multitask(model: nn.Module, classifier_name: str) -> MultiTaskModule:
     """ 
     Wraps around a model to make it a multitask model 
 
     :param model: model to be converted into MultiTaskModule
     :param classifier_name: the name of the attribute containing 
-                            the classification layer (nn.Linear). It can also be
-                            an instance of nn.Sequential containing multiple
+                            the classification layer (nn.Linear). It can also 
+                            be an instance of nn.Sequential containing multiple
                             layers as long as the classification layer is the
                             last layer.
     :return the decorated model, now subclassing MultiTaskModule, and
     accepting task_labels as forward() method argument
     """
     return MultiTaskDecorator(model, classifier_name)
+
 
 __all__ = ['as_multitask']
--- tests/test_helper_method.py
+++ tests/test_helper_method.py
@@ -3,13 +3,15 @@ import torch
 import torch.nn as nn
 from torch.optim import SGD
 import unittest
+import copy
 
-from avalanche.models.dynamic_modules import MultiTaskModule, MultiHeadClassifier
-from avalanche.models import SimpleCNN, SimpleMLP
-from avalanche.models.helper_method import as_multitask
+from avalanche.models.dynamic_modules import MultiTaskModule, \
+MultiHeadClassifier
+from avalanche.models import SimpleCNN, SimpleMLP, as_multitask
 from avalanche.training.strategies import Naive
 
 from tests.unit_tests_utils import common_setups, get_fast_benchmark
+
 
 class ConversionMethodTests(unittest.TestCase):
     def setUp(self):
@@ -17,12 +19,49 @@ class ConversionMethodTests(unittest.TestCase):
         self.benchmark = get_fast_benchmark(use_task_labels=True, shuffle=True)
 
     def test_modules(self):
-        modules = [(SimpleMLP(input_size=32*32*3), 'classifier'), (SimpleCNN(), 'classifier')]
-        for m in modules:self._test_modules(*m)
+        modules = [(SimpleMLP(input_size=32*32*3), 'classifier'),
+        (SimpleCNN(), 'classifier')]
+        for m in modules: 
+            self._test_modules(*m)
+
+    def test_outputs(self):
+        modules = [(SimpleMLP(input_size=32*32*3), 'classifier'),
+        (SimpleCNN(), 'classifier')]
+        for m in modules: 
+            self._test_outputs(*m)
 
     def test_integration(self):
         modules = [(SimpleMLP(input_size=6), 'classifier')]
-        for m in modules:self._test_integration(*m)
+        for m in modules: 
+            self._test_integration(*m)
+
+    def test_initialisation(self):
+        module = SimpleMLP()
+        old_classifier_weight = torch.clone(module.classifier.weight)
+        old_classifier_bias = torch.clone(module.classifier.bias)
+        module = as_multitask(module, 'classifier')
+        new_classifier_weight = \
+        torch.clone(module.classifier.classifiers['0'].classifier.weight)
+        new_classifier_bias = \
+        torch.clone(module.classifier.classifiers['0'].classifier.bias)
+        self.assertTrue(torch.equal(old_classifier_weight, 
+        new_classifier_weight))
+        self.assertTrue(torch.equal(old_classifier_bias, 
+        new_classifier_bias))
+
+    def _test_outputs(self, module, clf_name):
+        test_input = torch.rand(10, 3, 32, 32)
+        module_singletask = copy.deepcopy(module)
+        module_multitask = as_multitask(module, clf_name)
+
+        # Put in eval mode to deactivate dropouts
+        module_singletask.eval()
+        module_multitask.eval()
+
+        out_single_task = module_singletask(test_input)
+        out_multi_task = module_multitask(test_input, task_labels=0)
+        self.assertTrue(torch.equal(out_single_task, 
+        out_multi_task))
 
     def _test_modules(self, module, clf_name):
         old_param_total = sum([torch.numel(p) for p in module.parameters()])
@@ -72,3 +111,5 @@ class ConversionMethodTests(unittest.TestCase):
         for t, experience in enumerate(self.benchmark.train_stream):
             strategy.train(experience)
             strategy.eval(self.benchmark.test_stream[:t+1])
+
+
