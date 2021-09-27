4f811ca8398221d6ba0eaaaef72435f7976deda8
Antonio Carta <antonio.carta@di.unipi.it>
2021-09-27 11:50:52 +0200

    ADD top-level imports

--- avalanche/__init__.py
+++ avalanche/__init__.py
@@ -1,4 +1,10 @@
-import os
+from avalanche import benchmarks
+from avalanche import evaluation
+from avalanche import logging
+from avalanche import models
+from avalanche import training
+
+
 __version__ = "0.0.1"
 
 _dataset_add = None
@@ -21,23 +27,3 @@ def _avalanche_monkey_patches():
 
 
 _avalanche_monkey_patches()
-
-
-# Hotfix for MNIST download issue
-# def _adapt_dataset_urls():
-#     from torchvision.datasets import MNIST
-#     prev_mnist_urls = MNIST.resources
-#     new_resources = [
-#         ('https://storage.googleapis.com/cvdf-datasets/mnist/'
-#          'train-images-idx3-ubyte.gz', prev_mnist_urls[0][1]),
-#         ('https://storage.googleapis.com/cvdf-datasets/mnist/'
-#          'train-labels-idx1-ubyte.gz', prev_mnist_urls[1][1]),
-#         ('https://storage.googleapis.com/cvdf-datasets/mnist/'
-#          't10k-images-idx3-ubyte.gz', prev_mnist_urls[2][1]),
-#         ('https://storage.googleapis.com/cvdf-datasets/mnist/'
-#          't10k-labels-idx1-ubyte.gz', prev_mnist_urls[3][1])
-#     ]
-#     MNIST.resources = new_resources
-#
-#
-# _adapt_dataset_urls()
