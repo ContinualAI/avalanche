477c69e743caab8ec64767156958e897e258b863
nishantaswani@nyu.edu <nishantaswani@nyu.edu>
2022-06-03 22:25:43 +0400

    Add autoencoder strategy

--- avalanche/training/supervised/strategy_wrappers.py
+++ avalanche/training/supervised/strategy_wrappers.py
@@ -38,7 +38,9 @@ from avalanche.training.plugins import (
 )
 from avalanche.training.templates.base import BaseTemplate
 from avalanche.training.templates.supervised import SupervisedTemplate
+from avalanche.evaluation.metrics import accuracy_metrics, loss_metrics
 from avalanche.models.generator import MlpVAE, VAE_loss
+from avalanche.models.expert_gate import AE_loss
 from avalanche.logging import InteractiveLogger
 
 
@@ -475,6 +477,69 @@ class GenerativeReplay(SupervisedTemplate):
             eval_every=eval_every,
             **base_kwargs
         )
+
+
+class AETraining(SupervisedTemplate):
+
+    ae_evaluator = EvaluationPlugin(
+        loss_metrics(minibatch=False, epoch=False,
+                     experience=False, stream=True),
+        loggers=[InteractiveLogger()],
+        suppress_warnings=True,
+        )
+
+    def __init__(
+        self,
+        model: Module,
+        optimizer: Optimizer,
+        criterion=AE_loss,
+        train_mb_size: int = 1,
+        train_epochs: int = 1,
+        eval_mb_size: int = 1,
+        device=None,
+        plugins: Optional[List[SupervisedPlugin]] = None,
+        evaluator: EvaluationPlugin = ae_evaluator,
+        eval_every=0,
+        **base_kwargs
+    ):
+        """
+        Creates an instance of the Naive strategy.
+
+        :param model: The model.
+        :param optimizer: The optimizer to use.
+        :param criterion: The loss criterion to use.
+        :param train_mb_size: The train minibatch size. Defaults to 1.
+        :param train_epochs: The number of training epochs. Defaults to 1.
+        :param eval_mb_size: The eval minibatch size. Defaults to 1.
+        :param device: The device to use. Defaults to None (cpu).
+        :param plugins: Plugins to be added. Defaults to None.
+        :param evaluator: (optional) instance of EvaluationPlugin for logging
+            and metric computations.
+        :param eval_every: the frequency of the calls to `eval` inside the
+            training loop. -1 disables the evaluation. 0 means `eval` is called
+            only at the end of the learning experience. Values >0 mean that
+            `eval` is called every `eval_every` epochs and at the end of the
+            learning experience.
+        :param \*\*base_kwargs: any additional
+            :class:`~avalanche.training.BaseTemplate` constructor arguments.
+        """
+
+        super().__init__(
+            model,
+            optimizer,
+            criterion,
+            train_mb_size=train_mb_size,
+            train_epochs=train_epochs,
+            eval_mb_size=eval_mb_size,
+            device=device,
+            plugins=plugins,
+            evaluator=evaluator,
+            eval_every=eval_every,
+            **base_kwargs
+        )
+
+    def criterion(self):
+        return self._criterion(self.mb_x, self.mb_output)
 
 
 class VAETraining(SupervisedTemplate):
@@ -1269,6 +1334,7 @@ __all__ = [
     "CWRStar",
     "Replay",
     "GenerativeReplay",
+    "AETraining",
     "VAETraining",
     "GDumb",
     "LwF",
