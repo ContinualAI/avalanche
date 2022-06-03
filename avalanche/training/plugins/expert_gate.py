a67c76a750616c036fab1196dd8184b61a3b704d
nishantaswani@nyu.edu <nishantaswani@nyu.edu>
2022-06-03 22:21:21 +0400

    Remove expert_gate plugin init

--- avalanche/training/plugins/expert_gate.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-from avalanche.training.plugins.strategy_plugin import SupervisedPlugin
-from avalanche.models.base_model import BaseModel
-
-
-class ExpertGatePlugin(SupervisedPlugin):
-    """Expert Gate Plugin.
-    """
