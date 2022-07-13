5f31d140195a0581b06a3be46fb3f8984e3d5e56
Julio <julious.hurtado@gmail.com>
2022-07-13 18:41:57 -0400

    Add models used in BiC

--- avalanche/models/bic_model.py
+++ avalanche/models/bic_model.py
@@ -1,4 +1,8 @@
 import torch
+from avalanche.models.dynamic_modules import (
+    MultiTaskModule,
+    MultiHeadClassifier,
+)
 
 
 class BiCAdapter(torch.nn.Module):
@@ -9,7 +13,7 @@ class BiCAdapter(torch.nn.Module):
         self.bias_layers = []
 
     def add_bias_layer(self, device, cls):
-        self.bias_layers.append(BiasLayer(device, cls))
+        self.bias_layers.append(BiasLayer(device, cls, False))
     
     def forward(self, x):
         out = self.model(x)
@@ -23,23 +27,62 @@ class BiCAdapter(torch.nn.Module):
         return self.model(x)
 
 
+class BiCAdapterMH(MultiTaskModule):
+    def __init__(self, model) -> None:
+        super(BiCAdapterMH, self).__init__()
+
+        self.model = model
+        self.bias_layers = []
+
+        self.model.fc = MultiHeadClassifier(64)
+
+    def add_bias_layer(self, device, cls):
+        self.bias_layers.append(BiasLayer(device, cls, True))
+    
+    def forward_single_task(self, x, task_label):
+        out = self.model.forward_rep(x)
+        out = self.model.fc(out, task_label)
+        
+        if isinstance(task_label, int):
+            # fast path. mini-batch is single task.
+            return self.bias_layers[task_label](out)
+        else:
+            unique_tasks = torch.unique(task_label)
+
+            out = torch.zeros_like(out)
+
+            for task in unique_tasks:
+                task_mask = task_label == task
+                x_task = out[task_mask]
+                out[task_mask] = self.bias_layers[task](x_task)
+            return out
+
+    def forward_logits(self, x):
+        return self.model(x)
+
+
 class BiasLayer(torch.nn.Module):
     """Bias layers with alpha and beta parameters"""
 
-    def __init__(self, device, cls):
+    def __init__(self, device, cls, task_incremental=False):
         super(BiasLayer, self).__init__()
         self.alpha = torch.nn.Parameter(torch.ones(1, device=device))
         self.beta = torch.nn.Parameter(torch.zeros(1, device=device))
 
-        self.cls = list(cls)
+        self.cls = torch.Tensor(list(cls)).long().to(device)
         self.device = device
+        self.task_incremental = task_incremental
 
     def forward(self, x):
-        a = torch.ones(x.size(1), device=self.device)
-        a[self.cls] = self.alpha
+        if self.task_incremental:
+            a = self.alpha
+            b = self.beta
+        else:
+            a = torch.ones(x.size(1), device=self.device)
+            a[self.cls] = self.alpha
 
-        b = torch.ones(x.size(1), device=self.device)
-        b[self.cls] = self.beta
+            b = torch.ones(x.size(1), device=self.device)
+            b[self.cls] = self.beta
 
         # return self.alpha * x + self.beta
         return a * x + b
