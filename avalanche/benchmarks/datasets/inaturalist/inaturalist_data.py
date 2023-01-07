e1e738630680f1e414f476535c9bda0bec369d0f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-07 02:16:24 +0000

    Adding tarfile member sanitization to extractall()

--- avalanche/benchmarks/datasets/inaturalist/inaturalist_data.py
+++ avalanche/benchmarks/datasets/inaturalist/inaturalist_data.py
@@ -187,7 +187,26 @@ class INATURALIST_DATA(object):
                         os.path.join(self.data_folder, name[0]), "r:gz"
                     ) as tar:
                         self.log.info("Extracting INATURALIST images...")
-                        tar.extractall(self.data_folder)
+                        def is_within_directory(directory, target):
+                            
+                            abs_directory = os.path.abspath(directory)
+                            abs_target = os.path.abspath(target)
+                        
+                            prefix = os.path.commonprefix([abs_directory, abs_target])
+                            
+                            return prefix == abs_directory
+                        
+                        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                        
+                            for member in tar.getmembers():
+                                member_path = os.path.join(path, member.name)
+                                if not is_within_directory(path, member_path):
+                                    raise Exception("Attempted Path Traversal in Tar File")
+                        
+                            tar.extractall(path, members, numeric_owner) 
+                            
+                        
+                        safe_extract(tar, self.data_folder)
                         self.log.info("Done!")
                 else:
                     self.log.info("Skipping untarring, exists: ", save_name)
