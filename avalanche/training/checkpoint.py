e12552260e2099357b25e793f8017ae1f8914cbf
Antonio Carta <antonio.carta@di.unipi.it>
2023-04-11 18:16:12 +0200

    ADD documentation

---
+++ avalanche/training/checkpoint.py
@@ -0,0 +1,89 @@
+import os.path
+import sys
+import time
+from copy import copy
+
+import dill
+import torch
+
+from avalanche.training.determinism.rng_manager import RNGManager
+
+
+def maybe_load_checkpoint(strategy, fname, map_location=None):
+    """Load the strategy state from a checkpoint file.
+
+    The method returns the strategy with the state deserialized from the file
+    and the index of the training experience to resume training.
+
+    If the file does not exists, the method returns the strategy unmodified
+    and the index 0. As a result, the method can be safely called even if no
+    checkpoint has been previously created (e.g. during the first run).
+
+    Example:
+
+    ```
+    strategy = Naive(model, opt, train_mb_size=128)
+    strategy, initial_exp = maybe_load_checkpoint(strategy, fname)
+
+    for exp in benchmark.train_stream[initial_exp:]:
+        strategy.train(exp)
+        save_checkpoint(strat, fname)
+    ```
+
+    :param strategy: strategy to load. It must be already initialized.
+    :param fname: file name
+    :param map_location: sets the location of the tensors after serialization.
+        Same as `map_location` of `torch.load`, except that you can also pass
+        a device object or a string (a proper
+        map will be created accordingly). The recommended way to use this
+        parameter is to pass the used reference device.
+        In addition, all `torch.device` objects will be un-pickled using
+        that map (this is not usually done by `torch.load`,
+        but it is needed to properly manage things in Avalanche).
+        Defaults to None, which means that no mapping will take place.
+    :return: tuple <strategy, exp_counter>
+        strategy after deserialization,
+        index of the current experience to resume training.
+    """
+    if not os.path.exists(fname):
+        return None, 0
+
+    ckp = torch.load(fname, pickle_module=dill,
+                     map_location=map_location)
+
+    print(ckp)
+    strategy.__dict__.update(ckp['strategy'].__dict__)
+    exp_counter = ckp['exp_counter']
+    return strategy, exp_counter
+
+
+def save_checkpoint(strategy, fname, exclude=None):
+    """Save the strategy state into a file.
+
+    The file can be loaded using `maybe_load_checkpoint`.
+
+    For efficiency, the user can specify some attributes to `exclude`.
+    For example, if the optimizer is static and doesn't change during training,
+    it can be safely excluded. These helps to speed up the serialization.
+
+    WARNING: the method cannot be used inside the training and evaluation loops of the strategy.
+
+    :param strategy:
+    :param fname:
+    :param exclude: List[string] list of attributes to remove before the serialization.
+    :return:
+    """
+    if exclude is None:
+        exclude = []
+    ended_experience_counter = strategy.clock.train_exp_counter
+
+    strategy = copy(strategy)
+    for attr in exclude:
+        delattr(strategy, attr)
+
+    checkpoint_data = {
+        'strategy': strategy,
+        'rng_manager': RNGManager,
+        'exp_counter': ended_experience_counter
+    }
+    torch.save(checkpoint_data, fname, pickle_module=dill)
--- examples/checkpointing.py
+++ examples/checkpointing.py
@@ -4,7 +4,7 @@
 # See the accompanying LICENSE file for terms.                                 #
 #                                                                              #
 # Date: 15-09-2022                                                             #
-# Author(s): Lorenzo Pellegrini                                                #
+# Author(s): Lorenzo Pellegrini, Antonio Carta                                 #
 # E-mail: contact@continualai.org                                              #
 # Website: avalanche.continualai.org                                           #
 ################################################################################
@@ -31,6 +31,7 @@ from avalanche.evaluation.metrics import accuracy_metrics, loss_metrics, \
 from avalanche.logging import InteractiveLogger, TensorboardLogger, \
     WandBLogger, TextLogger
 from avalanche.models import SimpleMLP, as_multitask
+from avalanche.training.checkpoint import maybe_load_checkpoint, save_checkpoint
 from avalanche.training.determinism.rng_manager import RNGManager
 from avalanche.training.plugins import EvaluationPlugin, ReplayPlugin
 from avalanche.training.plugins.checkpoint import CheckpointPlugin, \
@@ -38,7 +39,7 @@ from avalanche.training.plugins.checkpoint import CheckpointPlugin, \
 from avalanche.training.supervised import Naive
 
 
-def main(args):
+def checkpointing_with_plugin(args):
     # FIRST CHANGE: SET THE RANDOM SEEDS
     # In fact, you should to this no matter the checkpointing functionality.
     # Remember to load checkpoints by setting the same random seed used when
@@ -172,6 +173,58 @@ def main(args):
             break
 
 
+def checkpointing_standalone(args):
+    # STEP 1: SET THE RANDOM SEEDS to guarantee reproducibility
+    RNGManager.set_random_seeds(1234)
+
+    # Nothing new here...
+    device = torch.device(
+        f"cuda:{args.cuda}"
+        if torch.cuda.is_available() and args.cuda >= 0
+        else "cpu"
+    )
+    print('Using device', device)
+
+    # CL Benchmark Creation (as usual)
+    benchmark = SplitMNIST(5)
+    model = SimpleMLP(input_size=28*28, num_classes=10)
+    optimizer = SGD(model.parameters(), lr=0.01, momentum=0.9)
+    criterion = CrossEntropyLoss()
+
+    # Create the evaluation plugin (as usual)
+    evaluation_plugin = EvaluationPlugin(
+        accuracy_metrics(experience=True, stream=True),
+        loggers=[InteractiveLogger()]
+    )
+
+    # Create the strategy (as usual)
+    strategy = Naive(
+        model=model,
+        optimizer=optimizer,
+        criterion=criterion,
+        train_mb_size=128,
+        train_epochs=2,
+        eval_mb_size=128,
+        device=device,
+        evaluator=evaluation_plugin
+    )
+
+    # STEP 2: TRY TO LOAD THE LAST CHECKPOINT
+    # if the checkpoint exists, load it into the newly created strategy
+    # the method also loads the experience counter, so we know where to
+    # resume training
+    fname = './checkpoint.pkl'  # name of the checkpoint file
+    strategy, initial_exp = maybe_load_checkpoint(strategy, fname)
+
+    # STEP 3: USE THE "initial_exp" to resume training
+    for train_exp in benchmark.train_stream[initial_exp:]:
+        strategy.train(train_exp, num_workers=4, persistent_workers=True)
+        strategy.eval(benchmark.test_stream, num_workers=4)
+
+        # STEP 4: SAVE the checkpoint after training on each experience.
+        save_checkpoint(strategy, fname)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -189,4 +242,4 @@ if __name__ == "__main__":
         "--wandb",
         action='store_true'
     )
-    main(parser.parse_args())
+    checkpointing_standalone(parser.parse_args())
