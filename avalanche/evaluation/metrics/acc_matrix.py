196c9360eda5f4fd9d1406d58defd448957e27b7
Julio Hurtado <julious.hurtado@gmail.com>
2023-05-23 11:35:40 +0200

    Add docstring to Accuracy MAtrix

--- avalanche/evaluation/metrics/acc_matrix.py
+++ avalanche/evaluation/metrics/acc_matrix.py
@@ -9,82 +9,110 @@ from avalanche.training.templates import SupervisedTemplate
 
 class AccuracyMatrixPluginMetric(PluginMetric[float]):
     """
-    Base class for all accuracies plugin metrics
+    Class for obtaining an Accuracy Matrix for the evaluation stream
     """
 
-    def __init__(self, mode="eval"):
-        """Creates the Accuracy plugin
-        :param mode: If it going to run in eval or train. For now only 
-            available in evaluation.
+    def __init__(self):
+        """
+        Creates the Accuracy Matrix plugin
         """
         self._accuracy = Accuracy()
-        self._mode = mode
         super(AccuracyMatrixPluginMetric, self).__init__()
 
         self.count = 0
         self.matrix = torch.zeros((1, 1))
-        self.last_task_id = 0
         self.online = False
 
-        self.current_task_id = None
-
-        if self._mode == 'train':
-            assert False, "This metric only works in eval"
+        self.num_training_steps = None
 
     def reset(self, strategy=None) -> None:
+        """Resets the metric.
+
+        :param strategy: The strategy object associated with the stream.
+        """
         self.matrix = torch.zeros((1, 1))
         self.count = 0
 
     def result(self, strategy=None) -> float:
+        """Returns the metric result.
+
+        :param strategy: The strategy object associated with the stream.
+        :return: The metric result as a torch tensor.
+        """
         return self.matrix
 
     def add_new_task(self, new_length):
+        """Adds a new dimension to the accuracy matrix.
+
+        :param new_length: The new dimension of the matrix. We assume a square 
+                            matrix
+        """
         temp = self.matrix.clone()
         self.matrix = torch.zeros((new_length, new_length))
         self.matrix[:temp.size(0), :temp.size(1)] = temp
 
-    def update(self, current_task_id, eval_task_id):
-        if (max(current_task_id, eval_task_id) + 1) > self.matrix.size(0):
-            self.add_new_task(max(current_task_id, eval_task_id)+1)
+    def update(self, num_training_steps, eval_exp_id):
+        """Updates the matrix with the accuracy value for a given task pair.
+
+        :param num_training_steps: The ID of the current training experience.
+        :param eval_exp_id: The ID of the evaluation experience.
+        """
+        if (max(num_training_steps, eval_exp_id) + 1) > self.matrix.size(0):
+            self.add_new_task(max(num_training_steps, eval_exp_id)+1)
 
         acc = self._accuracy.result()
-        self.matrix[current_task_id, eval_task_id] = acc
+        self.matrix[num_training_steps, eval_exp_id] = acc
 
         self._accuracy.reset()
 
     def _package_result(self, strategy: "SupervisedTemplate") -> "MetricResult":
+        """Packages the metric result.
+
+        :param strategy: The strategy object associated with the stream.
+        :return: The metric result. As a MetricValue object.
+        """
         metric_value = self.result(strategy)
         plot_x_position = strategy.clock.train_iterations
-
-        if self._mode == "train":
-            metric_name = "TrainStream/Acc_Matrix"
-        else:
-            metric_name = "EvalStream/Acc_Matrix"
+        metric_name = "EvalStream/Acc_Matrix"
 
         return [MetricValue(self, metric_name, metric_value, plot_x_position)]
 
     def after_eval_iteration(self, strategy: "SupervisedTemplate"):
+        """Performs actions after each evaluation iteration.
+
+        :param strategy: The strategy object associated with the stream.
+        """
         super().after_eval_iteration(strategy)
-        if self._mode == "eval":
-            self._accuracy.update(strategy.mb_output, strategy.mb_y)
+        self._accuracy.update(strategy.mb_output, strategy.mb_y)
 
     def after_eval_exp(self, strategy: "SupervisedTemplate"):
+        """Performs actions after evaluating an experience.
+
+        :param strategy: The strategy object associated with the stream.
+        """
         super().after_eval_exp(strategy)
-        if self._mode == "eval":
-            curr_exp = strategy.experience.current_experience
-            self.update(self.current_task_id, curr_exp)
+        curr_exp = strategy.experience.current_experience
+        self.update(self.num_training_steps, curr_exp)
 
     def after_eval(self, strategy: "SupervisedTemplate"):
+        """Performs actions after the evaluation phase.
+
+        :param strategy: The strategy object associated with the stream.
+        :return: The metric result.
+        """
         super().after_eval_exp(strategy)
-        if self._mode == "eval":
-            return self._package_result(strategy)
+        return self._package_result(strategy)
 
     def before_training(self, strategy: "SupervisedTemplate"):
+        """Performs actions before the training phase.
+
+        :param strategy: The strategy object associated with the metric.
+        """
         super().before_training(strategy)
-        if self.current_task_id is not None:
-            self.current_task_id += 1
+        if self.num_training_steps is not None:
+            self.num_training_steps += 1
         else:
-            self.current_task_id = 0     
+            self.num_training_steps = 0     
 
 
 def accuracy_matrix_metrics(
@@ -97,7 +125,7 @@ def accuracy_matrix_metrics(
     """
 
     metrics = []
-    metrics.append(AccuracyMatrixPluginMetric("eval"))
+    metrics.append(AccuracyMatrixPluginMetric())
 
     return metrics
 
