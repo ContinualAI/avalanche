2dc4447fa6dc33f775b1fea136e4cb365888a822
Hamed <hamed@eduroamverw-130-82-25-151.unisg.ch>
2023-06-05 14:07:03 +0200

    Remove CustomResNet from dualnet_model

--- avalanche/models/dualnet_model.py
+++ avalanche/models/dualnet_model.py
@@ -81,48 +81,6 @@ class BasicBlock(nn.Module):
         return out
 
 
-class CustomResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes, nf):
-        super(CustomResNet, self).__init__()
-        self.in_planes = nf
-
-        self.conv1 = conv3x3(3, nf * 1)
-        self.bn1 = nn.BatchNorm2d(nf * 1)
-        self.layer1 = self._make_layer(block, nf * 1, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, nf * 2, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, nf * 4, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, nf * 8, num_blocks[3], stride=2)
-        # self.linear = nn.Linear(nf * 8 * block.expansion, int(num_classes))
-        self.linear = NCM_classifier(nf*8*block.expansion)
-
-    def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1] * (num_blocks - 1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        bsz = x.size(0)
-        out = relu(self.bn1(self.conv1(x.view(bsz, 3, 32, 32))))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        # out = self.linear(out)
-        return out
-
-    def update_means(self, x, y, alpha=0):
-        self.linear.update_means(x, y, alpha=alpha)
-
-    def predict(self, x, t):
-        out = self.linear(x, t)
-        return out
-
-
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes, nf):
         super(ResNet, self).__init__()
@@ -164,10 +122,6 @@ class ResNet(nn.Module):
             return feat
         y = self.linear(feat) 
         return y
-
-
-def cResNet18(num_classes, nf=20):
-    return CustomResNet(BasicBlock, [2, 2, 2, 2], num_classes, nf)
 
 
 def ResNet18(num_classes, nf=20):
