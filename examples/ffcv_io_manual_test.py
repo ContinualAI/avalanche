b127a08ae5e6b1208386b74be7300aa40d5dfc22
Lorenzo Pellegrini <lrzpellegrini@gmail.com>
2023-07-07 16:18:32 +0200

    Additional linter fix.

--- examples/ffcv_io_manual_test.py
+++ examples/ffcv_io_manual_test.py
@@ -31,54 +31,57 @@ from torch.utils.data import DataLoader
 # %%
 def main(cuda: int):
     # --- CONFIG
-    device = torch.device(
-        f"cuda:{cuda}" if torch.cuda.is_available() else "cpu"
-    )
+    device = torch.device(f"cuda:{cuda}" if torch.cuda.is_available() else "cpu")
     RNGManager.set_random_seeds(1234)
 
     # Define here the transformations to check
-    train_transform = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(p=0.5),
-        transforms.ToTensor(),
-    ])
 
-    eval_transform = transforms.Compose([
-        transforms.ToTensor(),
-    ])
-
+    # CIFAR-100
+    # cifar_train_transform = transforms.Compose(
+    #     [
+    #         transforms.RandomCrop(32, padding=4),
+    #         transforms.RandomHorizontalFlip(p=0.5),
+    #         transforms.ToTensor(),
+    #     ]
+    # )
+    # cifar_eval_transform = transforms.Compose(
+    #     [
+    #         transforms.ToTensor(),
+    #     ]
+    # )
     # benchmark = SplitCIFAR100(
     #     5,
     #     seed=4321,
     #     shuffle=True,
-    #     train_transform=train_transform,
-    #     eval_transform=eval_transform
+    #     train_transform=cifar_train_transform,
+    #     eval_transform=cifar_eval_transform
     # )
     # write_dir = './ffcv_manual_test_cifar100'
 
+    # TinyImagenet
     benchmark = SplitTinyImageNet()
-    write_dir = './ffcv_manual_test_tiny_imagenet'
-    
+    write_dir = "./ffcv_manual_test_tiny_imagenet"
+
     # It is recommended to start with `None`, so that Avalanche can try
     # putting a pipeline together automatically by translating common
-    # transformations to FFCV.
+    # torchvision transformations to FFCV.
     # If you encounter issues or the output is not what you expect, then
     # it is recommended to start from the pipeline printed by Avalanche
-    # and change it following the guides in the FFCV website and repo.
+    # and adapt it by following the guides in the FFCV website and repo.
     custom_decoder_pipeline = None
 
     num_workers = 8
 
-    print('Preparing FFCV datasets...')
+    print("Preparing FFCV datasets...")
     prepare_ffcv_datasets(
         benchmark=benchmark,
         write_dir=write_dir,
         device=device,
         ffcv_parameters=dict(num_workers=num_workers),
         decoder_def=custom_decoder_pipeline,
-        print_summary=True  # Leave to True to get important info!
+        print_summary=True,  # Leave to True to get important info!
     )
-    print('FFCV datasets ready')
+    print("FFCV datasets ready")
 
     # Create the FFCV Loader
     start_time = time.time()
@@ -86,33 +89,28 @@ def main(cuda: int):
         benchmark.train_stream[0].dataset,
         batch_sampler=None,
         batch_size=12,
-        ffcv_loader_parameters=dict(
-            num_workers=num_workers,
-            drop_last=True
-        ),
+        ffcv_loader_parameters=dict(num_workers=num_workers, drop_last=True),
         device=device,
         persistent_workers=False,
         print_ffcv_summary=True,
-        start_immediately=False
+        start_immediately=False,
     )
     end_time = time.time()
-    print('Loader creation took', end_time - start_time, 'seconds')
+    print("Loader creation took", end_time - start_time, "seconds")
 
     # Also load the same data using a PyTorch DataLoader
     # Note: data will be different when using random augmentations!
     pytorch_loader = DataLoader(
-        benchmark.train_stream[0].dataset,
-        batch_size=12,
-        drop_last=True
+        benchmark.train_stream[0].dataset, batch_size=12, drop_last=True
     )
 
     start_time = time.time()
     for i, (ffcv_batch, torch_batch) in enumerate(
         zip(ffcv_data_loader, pytorch_loader)
     ):
-        print(f'Batch {i} composition (FFCV vs PyTorch)')
+        print(f"Batch {i} composition (FFCV vs PyTorch)")
         for element in ffcv_batch:
-            print(element.shape, 'vs', element.shape)
+            print(element.shape, "vs", element.shape)
 
         n_to_show = 3
         for idx in range(n_to_show):
@@ -125,15 +123,15 @@ def main(cuda: int):
             ffcv_task = ffcv_batch[2][idx].item()
             torch_task = torch_batch[2][idx].item()
             f.suptitle(
-                f'Label: {ffcv_label}/{torch_label}, '
-                f'Task label: {ffcv_task}/{torch_task}'
+                f"Label: {ffcv_label}/{torch_label}, "
+                f"Task label: {ffcv_task}/{torch_task}"
             )
 
-            axarr[0].set_title('FFCV')
+            axarr[0].set_title("FFCV")
             axarr[0].imshow(as_img_ffcv)
-            axarr[1].set_title('PyTorch')
+            axarr[1].set_title("PyTorch")
             axarr[1].imshow(as_img_torch)
-            
+
             plt.show()
             f.clear()
 
@@ -157,20 +155,20 @@ def main(cuda: int):
                 almost_same = almost_same and (
                     torch.sum(torch.abs(f - t) > 1e-6).item() == 0
                 )
-        
-        print('all_same', all_same)
-        print('almost_same', almost_same)
-        print('correct_device', correct_device)
+
+        print("all_same", all_same)
+        print("almost_same", almost_same)
+        print("correct_device", correct_device)
         # ---------------------------------------------
-        
+
         # Keep this break if it is sufficient to analyze only the first batch
-        break 
+        break
 
         # Print batch separator
-        print('.' * 40)
+        print("." * 40)
 
     end_time = time.time()
-    print('Loop time:', end_time - start_time, 'seconds')
+    print("Loop time:", end_time - start_time, "seconds")
 
 
 # When running on VSCode (with Python extension), you will notice additional
