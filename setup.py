a9f8ae8b32584992f3990477e27f736e4010e58c
Antonio Carta <antonio.carta@di.unipi.it>
2024-01-23 16:14:42 +0100

    update python version requirements

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7,<3.11",
+    python_requires=">=3.8",
     install_requires=[
         "typing-extensions==4.4.0",
         "psutil",
