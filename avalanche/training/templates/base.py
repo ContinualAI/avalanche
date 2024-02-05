4d15accd1f7ec8f0a13364f9a85451e348e2bd3c
Lorenzo Pellegrini <lrzpellegrini@gmail.com>
2024-02-05 17:45:09 +0100

    Fix lint issues

--- avalanche/training/templates/base.py
+++ avalanche/training/templates/base.py
@@ -148,9 +148,9 @@ class BaseTemplate(BaseStrategyProtocol[TExperienceType]):
         self.model.to(self.device)
 
         # Normalize training and eval data.
-        experiences_list: Iterable[TExperienceType] = (
-            _experiences_parameter_as_iterable(experiences)
-        )
+        experiences_list: Iterable[
+            TExperienceType
+        ] = _experiences_parameter_as_iterable(experiences)
 
         if eval_streams is None:
             eval_streams = [experiences_list]
@@ -202,9 +202,9 @@ class BaseTemplate(BaseStrategyProtocol[TExperienceType]):
         self.is_training = False
         self.model.eval()
 
-        experiences_list: Iterable[TExperienceType] = (
-            _experiences_parameter_as_iterable(experiences)
-        )
+        experiences_list: Iterable[
+            TExperienceType
+        ] = _experiences_parameter_as_iterable(experiences)
         self.current_eval_stream = experiences_list
 
         self._before_eval(**kwargs)
